8fca70e76bdd643e246c103057c77c94f79fed8f
Mike McQuaid <mike@mikemcquaid.com>
2019-01-30 21:29:21 +0000

    --cache: use CLI::Parser.

--- Library/Homebrew/cmd/--cache.rb
+++ Library/Homebrew/cmd/--cache.rb
@@ -1,15 +1,29 @@
-#:  * `--cache`:
-#:    Display Homebrew's download cache. See also `HOMEBREW_CACHE`.
-#:
-#:  * `--cache` [`--build-from-source`|`-s`] [`--force-bottle`] <formula>:
-#:    Display the file or directory used to cache <formula>.
-
 require "fetch"
+require "cli_parser"
 
 module Homebrew
   module_function
 
+  def __cache_args
+    Homebrew::CLI::Parser.new do
+      usage_banner <<~EOS
+        `--cache` [<options>] [<formula>]
+
+        Display Homebrew's download cache. See also `HOMEBREW_CACHE`.
+
+        If <formula> is provided, display the file or directory used to cache <formula>.
+      EOS
+      switch "-s", "--build-from-source",
+        description: "Show the cache file used when building from source."
+      switch "--force-bottle",
+        description: "Show the cache file used when pouring a bottle."
+      conflicts "--build-from-source", "--force-bottle"
+    end
+  end
+
   def __cache
+    __cache_args.parse
+
     if ARGV.named.empty?
       puts HOMEBREW_CACHE
     else
