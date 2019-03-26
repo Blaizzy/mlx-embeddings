1e5cfcc028b7d6073717fd84e3ae5caab033300b
Mike McQuaid <mike@mikemcquaid.com>
2019-03-26 09:49:13 +0000

    Separate tap tests into separate, correct files.

---
+++ Library/Homebrew/test/cmd/untap_spec.rb
@@ -0,0 +1,16 @@
+require "cmd/shared_examples/args_parse"
+
+describe "Homebrew.untap_args" do
+  it_behaves_like "parseable arguments"
+end
+
+describe "brew untap", :integration_test do
+  it "untaps a given Tap" do
+    setup_test_tap
+
+    expect { brew "untap", "homebrew/foo" }
+      .to output(/Untapped/).to_stdout
+      .and not_to_output.to_stderr
+      .and be_a_success
+  end
+end
