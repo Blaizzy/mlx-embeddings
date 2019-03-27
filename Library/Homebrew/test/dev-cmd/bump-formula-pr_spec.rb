cc6c6afffbfefbd92cb9f6f677c9f6f93f3b8c5c
Mike McQuaid <mike@mikemcquaid.com>
2019-03-27 14:22:30 +0000

    test/dev-cmd: add more parseable arguments tests.

---
+++ Library/Homebrew/test/dev-cmd/bump-formula-pr_spec.rb
@@ -0,0 +1,5 @@
+require "cmd/shared_examples/args_parse"
+
+describe "Homebrew.bump_formula_pr_args" do
+  it_behaves_like "parseable arguments"
+end
---
+++ Library/Homebrew/test/dev-cmd/release-notes_spec.rb
@@ -0,0 +1,5 @@
+require "cmd/shared_examples/args_parse"
+
+describe "Homebrew.release_notes_args" do
+  it_behaves_like "parseable arguments"
+end
