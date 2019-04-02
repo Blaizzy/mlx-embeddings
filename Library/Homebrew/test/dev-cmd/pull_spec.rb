fb643a28f010090ba3b794189dd0df0c09554910
Michka Popoff <michkapopoff@gmail.com>
2019-04-02 08:01:23 +0200

    pull_spec: fix test

--- Library/Homebrew/test/dev-cmd/pull_spec.rb
+++ Library/Homebrew/test/dev-cmd/pull_spec.rb
@@ -8,15 +8,13 @@ end
 
 describe "brew pull", :integration_test do
   it "fetches a patch from a GitHub commit or pull request and applies it", :needs_network do
-    CoreTap.instance.path.cd do
+    HOMEBREW_REPOSITORY.cd do
       system "git", "init"
-      system "git", "checkout", "-b", "new-branch"
-      system "git", "remote", "add", "origin", "https://github.com/Homebrew/homebrew-core"
+      system "git", "remote", "add", "origin", "https://github.com/Homebrew/brew"
     end
 
     expect { brew "pull", "https://github.com/Homebrew/brew/pull/1249" }
       .to output(/Fetching patch/).to_stdout
-      .and output(/Patch failed to apply/).to_stderr
       .and be_a_failure
   end
 end
