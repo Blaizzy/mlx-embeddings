e9806b6b55afa5102b253c628c0d1c90297c5e23
Mike McQuaid <mike@mikemcquaid.com>
2019-04-17 19:42:27 +0900

    cli/args: add class to handle arguments.

---
+++ Library/Homebrew/cli/args.rb
@@ -0,0 +1,15 @@
+require "ostruct"
+
+module Homebrew
+  module CLI
+    class Args < OpenStruct
+      # undefine tap to allow --tap argument
+      undef tap
+
+      def initialize(argv:)
+        super
+        @argv = argv
+      end
+    end
+  end
+end
