888c38480188ac8b1772e85e636ee33c8c9ed252
Mike McQuaid <mike@mikemcquaid.com>
2019-04-17 17:00:24 +0900

    release-notes: use stderr for header output.
    This makes the use of `brew release-notes | pbcopy` a bit easier.

--- Library/Homebrew/dev-cmd/release-notes.rb
+++ Library/Homebrew/dev-cmd/release-notes.rb
@@ -49,7 +49,7 @@ module Homebrew
       end
     end
 
-    puts "Release notes between #{previous_tag} and #{end_ref}:"
+    $stderr.puts "Release notes between #{previous_tag} and #{end_ref}:"
     puts output
   end
 end
