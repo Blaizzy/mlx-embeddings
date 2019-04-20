ce15622584618802a229960d20199cd1655cf5fa
Markus Reiter <me@reitermark.us>
2019-04-20 23:56:55 +0200

    Add `*_to_slashes` methods to `compat`.

---
+++ Library/Homebrew/compat.rb
@@ -0,0 +1,1 @@
+require "compat/cask/dsl/version"
---
+++ Library/Homebrew/compat/cask/dsl/version.rb
@@ -0,0 +1,24 @@
+module Cask
+  class DSL
+    class Version < ::String
+      module Compat
+        def dots_to_slashes
+          odeprecated "#dots_to_slashes"
+          version { tr(".", "/") }
+        end
+
+        def hyphens_to_slashes
+          odeprecated "#hyphens_to_slashes"
+          version { tr("-", "/") }
+        end
+
+        def underscores_to_slashes
+          odeprecated "#underscores_to_slashes"
+          version { tr("_", "/") }
+        end
+      end
+
+      prepend Compat
+    end
+  end
+end
