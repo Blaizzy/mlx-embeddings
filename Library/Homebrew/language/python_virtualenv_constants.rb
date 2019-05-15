0bb29e82533ceba1fa3bdc4b4c38d4201928f53d
Chongyu Zhu <i@lembacon.com>
2019-05-15 20:05:37 +0800

    python_virtualenv_constants: upgrade virtualenv to 16.6.0

--- Library/Homebrew/language/python_virtualenv_constants.rb
+++ Library/Homebrew/language/python_virtualenv_constants.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 PYTHON_VIRTUALENV_URL =
-  "https://files.pythonhosted.org/packages/6a/56" \
-  "/74dce1fdeeabbcc0a3fcf299115f6814bd9c39fc4161658b513240a75ea7" \
-  "/virtualenv-16.5.0.tar.gz"
+  "https://files.pythonhosted.org/packages/53/c0" \
+  "/c7819f0bb2cf83e1b4b0d96c901b85191f598a7b534d297c2ef6dc80e2d3" \
+  "/virtualenv-16.6.0.tar.gz"
 PYTHON_VIRTUALENV_SHA256 =
-  "15ee248d13e4001a691d9583948ad3947bcb8a289775102e4c4aa98a8b7a6d73"
+  "99acaf1e35c7ccf9763db9ba2accbca2f4254d61d1912c5ee364f9cc4a8942a0"
