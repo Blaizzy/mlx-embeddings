c3f7b7f94c04ba01076f16a7e5e3ccb616f0e8f0
Mike McQuaid <mike@mikemcquaid.com>
2019-05-20 16:58:14 +0100

    brew vendor-gems: commit updates.

--- Library/Homebrew/vendor/bundle/ruby/2.3.0/gems/rubocop-performance-1.2.0/lib/rubocop/performance/version.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module RuboCop
-  module Performance
-    module Version
-      STRING = '1.2.0'.freeze
-    end
-  end
-end
---
+++ Library/Homebrew/vendor/bundle/ruby/2.3.0/gems/rubocop-performance-1.3.0/lib/rubocop/performance/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Performance
+    module Version
+      STRING = '1.3.0'
+    end
+  end
+end
