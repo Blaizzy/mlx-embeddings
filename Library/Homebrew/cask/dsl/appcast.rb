0939c8832d9a12b436bc570240ab29e9f26a0298
Julian Mayer <julian@corecode.io>
2019-05-21 12:07:25 +0200

    Add configuration to new appcast check

--- Library/Homebrew/cask/dsl/appcast.rb
+++ Library/Homebrew/cask/dsl/appcast.rb
@@ -3,11 +3,21 @@
 module Cask
   class DSL
     class Appcast
+      ATTRIBUTES = [
+        :configuration,
+      ].freeze
       attr_reader :uri, :parameters
+      attr_reader(*ATTRIBUTES)
 
       def initialize(uri, **parameters)
         @uri        = URI(uri)
         @parameters = parameters
+
+        ATTRIBUTES.each do |attribute|
+          next unless parameters.key?(attribute)
+
+          instance_variable_set("@#{attribute}", parameters[attribute])
+        end
       end
 
       def to_yaml
