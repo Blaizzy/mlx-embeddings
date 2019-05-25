7a9bfb4c9f700033878e38ec4038da827744a9f6
Gabriel <ggkpacker@gmail.com>
2019-05-24 21:48:26 -0300

    Fix spec

--- Library/Homebrew/test/os/mac/software_spec_spec.rb
+++ Library/Homebrew/test/os/mac/software_spec_spec.rb
@@ -3,6 +3,8 @@
 require "software_spec"
 
 describe SoftwareSpec do
+  subject(:spec) { described_class.new }
+
   describe "#uses_from_macos" do
     before do
       sierra_os_version = OS::Mac::Version.from_symbol(:sierra)
@@ -12,36 +14,36 @@ describe SoftwareSpec do
     end
 
     it "doesn't adds a dependency if it doesn't meet OS version requirements" do
-      subject.uses_from_macos("foo", after: :high_sierra)
-      subject.uses_from_macos("bar", before: :el_capitan)
+      spec.uses_from_macos("foo", after: :high_sierra)
+      spec.uses_from_macos("bar", before: :el_capitan)
 
-      expect(subject.deps).to be_empty
+      expect(spec.deps).to be_empty
     end
 
     it "allows specifying dependencies after certain version" do
-      subject.uses_from_macos("foo", after: :el_capitan)
+      spec.uses_from_macos("foo", after: :el_capitan)
 
-      expect(subject.deps.first.name).to eq("foo")
+      expect(spec.deps.first.name).to eq("foo")
     end
 
     it "works with tags" do
-      subject.uses_from_macos("foo" => :head, after: :el_capitan)
+      spec.uses_from_macos("foo" => :head, :after => :el_capitan)
 
-      dep = subject.deps.first
+      dep = spec.deps.first
 
       expect(dep.name).to eq("foo")
       expect(dep.tags).to include(:head)
     end
 
     it "allows specifying dependencies before certain version" do
-      subject.uses_from_macos("foo", before: :high_sierra)
+      spec.uses_from_macos("foo", before: :high_sierra)
 
-      expect(subject.deps.first.name).to eq("foo")
+      expect(spec.deps.first.name).to eq("foo")
     end
 
     it "raises an error if passing invalid OS versions" do
       expect {
-        subject.uses_from_macos("foo", after: "bar", before: :mojave)
+        spec.uses_from_macos("foo", after: "bar", before: :mojave)
       }.to raise_error(ArgumentError, 'unknown version "bar"')
     end
   end
