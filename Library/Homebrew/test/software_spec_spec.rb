678f89ff210467f2e1d5302b62932227faa4da01
Gabriel <ggkpacker@gmail.com>
2019-05-30 13:18:54 -0300

    Fix typo

--- Library/Homebrew/test/software_spec_spec.rb
+++ Library/Homebrew/test/software_spec_spec.rb
@@ -134,25 +134,25 @@ describe SoftwareSpec do
 
   describe "#uses_from_macos" do
     it "allows specifying dependencies", :needs_linux do
-      spec.uses_from_macos("foo")
+      subject.uses_from_macos("foo")
 
-      expect(spec.deps.first.name).to eq("foo")
+      expect(subject.deps.first.name).to eq("foo")
     end
 
     it "works with tags", :needs_linux do
-      spec.uses_from_macos("foo" => :head, :after => :mojave)
+      subject.uses_from_macos("foo" => :head, :after => :mojave)
 
-      expect(spec.deps.first.name).to eq("foo")
-      expect(spec.deps.first.tags).to include(:head)
+      expect(subject.deps.first.name).to eq("foo")
+      expect(subject.deps.first.tags).to include(:head)
     end
 
     it "ignores OS version specifications", :needs_linux do
-      spec.uses_from_macos("foo", after: :mojave)
-      spec.uses_from_macos("bar" => :head, :after => :mojave)
+      subject.uses_from_macos("foo", after: :mojave)
+      subject.uses_from_macos("bar" => :head, :after => :mojave)
 
-      expect(spec.deps.first.name).to eq("foo")
-      expect(spec.deps.last.name).to eq("bar")
-      expect(spec.deps.last.tags).to include(:head)
+      expect(subject.deps.first.name).to eq("foo")
+      expect(subject.deps.last.name).to eq("bar")
+      expect(subject.deps.last.tags).to include(:head)
     end
   end
 
