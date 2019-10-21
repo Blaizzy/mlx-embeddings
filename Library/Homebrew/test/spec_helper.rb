1ad0addbc8ae015cfef4bb05101302e332f8f1f8
Steven Peters <scpeters@openrobotics.org>
2019-10-21 12:27:39 -0700

    spec_helper: always remove Frameworks

--- Library/Homebrew/test/spec_helper.rb
+++ Library/Homebrew/test/spec_helper.rb
@@ -185,6 +185,7 @@ RSpec.configure do |config|
         HOMEBREW_PINNED_KEGS,
         HOMEBREW_PREFIX/"var",
         HOMEBREW_PREFIX/"Caskroom",
+        HOMEBREW_PREFIX/"Frameworks",
         HOMEBREW_LIBRARY/"Taps/homebrew/homebrew-cask",
         HOMEBREW_LIBRARY/"Taps/homebrew/homebrew-bar",
         HOMEBREW_LIBRARY/"Taps/homebrew/homebrew-bundle",
@@ -198,11 +199,6 @@ RSpec.configure do |config|
         CoreTap.instance.path/"formula_renames.json",
         *Pathname.glob("#{HOMEBREW_CELLAR}/*/"),
       ]
-      if OS.mac?
-        FileUtils.rm_rf [
-          HOMEBREW_PREFIX/"Frameworks",
-        ]
-      end
 
       files_after_test = find_files
 
