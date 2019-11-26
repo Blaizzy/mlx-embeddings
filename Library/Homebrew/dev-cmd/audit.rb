e704bf7184bb8ab87acbb61dfabce3772bd326a0
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-11-26 10:26:17 +0000

    audit: add libepoxy 1.5.4 to whitelist

--- Library/Homebrew/dev-cmd/audit.rb
+++ Library/Homebrew/dev-cmd/audit.rb
@@ -756,6 +756,7 @@ module Homebrew
         gtk-mac-integration 2.1.3
         gtk-doc 1.31
         gcab 1.3
+        libepoxy 1.5.4
       ].each_slice(2).to_a.map do |formula, version|
         [formula, version.split(".")[0..1].join(".")]
       end
