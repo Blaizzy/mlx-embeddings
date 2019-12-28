073685284fe3f4fc9ac2a73b3e7fd99083a0b22b
Gautham Goli <gauthamgoli@gautham2.local>
2019-12-28 14:48:34 +0530

    postinstall: Use CLI::Parser to parse args

--- Library/Homebrew/postinstall.rb
+++ Library/Homebrew/postinstall.rb
@@ -6,15 +6,24 @@ require "global"
 require "debrew"
 require "fcntl"
 require "socket"
+require "cli/parser"
+
+def postinstall_args
+  Homebrew::CLI::Parser.new do
+    switch :force
+    switch :verbose
+    switch :debug
+  end
+end
 
 begin
+  postinstall_args.parse
   error_pipe = UNIXSocket.open(ENV["HOMEBREW_ERROR_PIPE"], &:recv_io)
   error_pipe.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
 
   trap("INT", old_trap)
 
-  formula = ARGV.resolved_formulae.first
-  formula.extend(Debrew::Formula) if ARGV.debug?
+  formula = Homebrew.args.resolved_formulae.first
   formula.run_post_install
 rescue Exception => e # rubocop:disable Lint/RescueException
   error_pipe.puts e.to_json
