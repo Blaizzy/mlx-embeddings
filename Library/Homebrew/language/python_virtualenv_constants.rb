38feac473c559be55866ae490dc255149e244856
Michka Popoff <michkapopoff@gmail.com>
2019-12-30 20:02:49 +0100

    virtualenv: support mojave and high sierra system Python
    On High Sierra and Mojave, virtualenv 16.x does not build when used with
    system Python
    Fall back to an older virtualenv version, which is known to work.

--- Library/Homebrew/language/python_virtualenv_constants.rb
+++ Library/Homebrew/language/python_virtualenv_constants.rb
@@ -6,3 +6,10 @@ PYTHON_VIRTUALENV_URL =
   "/virtualenv-16.7.4.tar.gz"
 PYTHON_VIRTUALENV_SHA256 =
   "94a6898293d07f84a98add34c4df900f8ec64a570292279f6d91c781d37fd305"
+
+PYTHON_VIRTUALENV_URL_MOJAVE =
+  "https://files.pythonhosted.org/packages/b1/72" \
+  "/2d70c5a1de409ceb3a27ff2ec007ecdd5cc52239e7c74990e32af57affe9" \
+  "/virtualenv-15.2.0.tar.gz"
+PYTHON_VIRTUALENV_SHA256_MOJAVE =
+  "1d7e241b431e7afce47e77f8843a276f652699d1fa4f93b9d8ce0076fd7b0b54"
