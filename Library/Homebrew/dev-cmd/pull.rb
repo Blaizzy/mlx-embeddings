9fb1876101a1abac8e3bfcc9f4f6fadbf0921c7c
Dawid Dziurla <dawidd0811@gmail.com>
2020-02-22 21:02:23 +0100

    pull: use `homebrew` remote if HOMEBREW_FORCE_HOMEBREW_ON_LINUX

--- Library/Homebrew/dev-cmd/pull.rb
+++ Library/Homebrew/dev-cmd/pull.rb
@@ -287,7 +287,10 @@ module Homebrew
     pr_number = url[%r{/pull\/([0-9]+)}, 1]
     return false unless pr_number
 
-    safe_system "git", "fetch", "--quiet", "origin", "pull/#{pr_number}/head"
+    # Use `homebrew` remote if HOMEBREW_FORCE_HOMEBREW_ON_LINUX env variable is set.
+    # The `homebrew` remote points to homebrew-core tap and is used by Linux maintainers.
+    remote = ENV["HOMEBREW_FORCE_HOMEBREW_ON_LINUX"] ? "homebrew" : "origin"
+    safe_system "git", "fetch", "--quiet", remote, "pull/#{pr_number}/head"
     Utils.popen_read("git", "rev-list", "--parents", "-n1", "FETCH_HEAD").count(" ") > 1
   end
 
