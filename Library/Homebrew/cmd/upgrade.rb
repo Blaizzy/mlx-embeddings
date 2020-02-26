da5d804bd9fca474eb2a7f484559ad9cd1534d89
Boris Klimenko <2@borisklimenko.ru>
2020-02-26 13:42:18 +0300

    Inline versions of upgraded formulae

--- Library/Homebrew/cmd/upgrade.rb
+++ Library/Homebrew/cmd/upgrade.rb
@@ -185,7 +185,11 @@ module Homebrew
     end
     fi.prelude
 
-    oh1 "Upgrading #{Formatter.identifier(f.full_specified_name)} #{fi.options.to_a.join " "}"
+    if f.optlinked?
+      oh1 "Upgrading #{Formatter.identifier(f.full_specified_name)} #{Keg.new(f.opt_prefix).version} -> #{f.pkg_version} #{fi.options.to_a.join " "}"
+    else
+      oh1 "Upgrading #{Formatter.identifier(f.full_specified_name)} -> #{f.pkg_version} #{fi.options.to_a.join " "}"
+    end
 
     # first we unlink the currently active keg for this formula otherwise it is
     # possible for the existing build to interfere with the build we are about to
