d0896d641ad1d27c2d3396087f3c27076027d116
Bo Anderson <mail@boanderson.me>
2020-03-05 03:40:23 +0000

    extend/string: match multiline make variables

--- Library/Homebrew/extend/string.rb
+++ Library/Homebrew/extend/string.rb
@@ -41,7 +41,7 @@ module StringInreplaceExtension
   # Looks for Makefile style variable definitions and replaces the
   # value with "new_value", or removes the definition entirely.
   def change_make_var!(flag, new_value)
-    return if gsub!(/^#{Regexp.escape(flag)}[ \t]*[\\?\+\:\!]?=[ \t]*(.*)$/, "#{flag}=#{new_value}", false)
+    return if gsub!(/^#{Regexp.escape(flag)}[ \t]*[\\?\+\:\!]?=[ \t]*((?:.*\\\n)*.*)$/, "#{flag}=#{new_value}", false)
 
     errors << "expected to change #{flag.inspect} to #{new_value.inspect}"
   end
@@ -50,7 +50,7 @@ module StringInreplaceExtension
   def remove_make_var!(flags)
     Array(flags).each do |flag|
       # Also remove trailing \n, if present.
-      unless gsub!(/^#{Regexp.escape(flag)}[ \t]*[\\?\+\:\!]?=.*$\n?/, "", false)
+      unless gsub!(/^#{Regexp.escape(flag)}[ \t]*[\\?\+\:\!]?=(?:.*\\\n)*.*$\n?/, "", false)
         errors << "expected to remove #{flag.inspect}"
       end
     end
@@ -58,6 +58,6 @@ module StringInreplaceExtension
 
   # Finds the specified variable
   def get_make_var(flag)
-    self[/^#{Regexp.escape(flag)}[ \t]*[\\?\+\:\!]?=[ \t]*(.*)$/, 1]
+    self[/^#{Regexp.escape(flag)}[ \t]*[\\?\+\:\!]?=[ \t]*((?:.*\\\n)*.*)$/, 1]
   end
 end
