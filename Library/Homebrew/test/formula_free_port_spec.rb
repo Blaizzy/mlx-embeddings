bfcba0f5f0a4c039939441c6f257b771f054c649
Alexander Bayandin <a.bayandin@gmail.com>
2020-03-27 14:29:50 +0000

    Library/Homebrew: add free_port test helper
    Co-Authored-By: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Homebrew/test/formula_free_port_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "socket"
+require "formula_free_port"
+
+module Homebrew
+  describe FreePort do
+    include described_class
+
+    describe "#free_port" do
+      # IANA suggests user port from 1024 to 49151
+      # and dynamic port for 49152 to 65535
+      # http://www.iana.org/assignments/port-numbers
+      MIN_PORT = 1024
+      MAX_PORT = 65535
+
+      it "returns a free TCP/IP port" do
+        port = free_port
+
+        expect(port).to be_between(MIN_PORT, MAX_PORT)
+        expect { TCPServer.new(port).close }.not_to raise_error
+      end
+    end
+  end
+end
