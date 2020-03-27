3955e7e13b980ad4f8d38ffc3c0253d86204902e
Bo Anderson <mail@boanderson.me>
2020-03-27 19:18:05 +0000

    language/test: add shebang tests

--- Library/Homebrew/test/language/python_spec.rb
+++ Library/Homebrew/test/language/python_spec.rb
@@ -2,6 +2,7 @@
 
 require "language/python"
 require "resource"
+require "utils/shebang"
 
 describe Language::Python, :needs_python do
   describe "#major_minor_version" do
@@ -28,6 +29,48 @@ describe Language::Python, :needs_python do
     it "can determine user site packages location" do
       expect(subject).to receive(:user_site_packages).and_return(Pathname)
       subject.user_site_packages("python")
+    end
+  end
+end
+
+describe Language::Python::Shebang do
+  let(:file) { Tempfile.new("python-shebang") }
+  let(:python_f) do
+    formula "python" do
+      url "https://brew.sh/python-1.0.tgz"
+    end
+  end
+  let(:f) do
+    formula "foo" do
+      url "https://brew.sh/foo-1.0.tgz"
+
+      depends_on "python"
+    end
+  end
+
+  before do
+    file.write <<~EOS
+      #!/usr/bin/env python3
+      a
+      b
+      c
+    EOS
+    file.flush
+  end
+
+  after { file.unlink }
+
+  describe "#detected_python_shebang" do
+    it "can be used to replace Python shebangs" do
+      expect(Formulary).to receive(:factory).with(python_f.name).and_return(python_f)
+      Utils::Shebang.rewrite_shebang described_class.detected_python_shebang(f), file
+
+      expect(File.read(file)).to eq <<~EOS
+        #!#{HOMEBREW_PREFIX}/opt/python/bin/python3
+        a
+        b
+        c
+      EOS
     end
   end
 end
