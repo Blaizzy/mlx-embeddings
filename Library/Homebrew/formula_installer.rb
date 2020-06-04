ad3acf800bceba1716d504bb84a260831346db92
Issy Long <me@issyl0.co.uk>
2020-06-04 22:46:47 +0100

    formula_installer: Fix `.installed?` method deprecation
    - Trying to test out a user-submitted `brew bump-formula-pr` for
      `app-engine-java` gave an error locally that [hasn't shown up on
      CI](https://github.com/Homebrew/homebrew-core/pull/55798/checks?check_run_id=740165542),
      oddly.
    
    ```
    ➜ brew install app-engine-java -s && brew test app-engine-java
    Error: Calling Formula#installed? is deprecated! Use Formula#latest_version_installed? (or Formula#any_version_installed? ) instead.
    /usr/local/Homebrew/Library/Homebrew/compat/formula.rb:6:in `installed?'
    /usr/local/Homebrew/Library/Homebrew/formula_installer.rb:421:in `block (2 levels) in check_requirements'
    /usr/local/Homebrew/Library/Homebrew/formula_installer.rb:420:in `each'
    /usr/local/Homebrew/Library/Homebrew/formula_installer.rb:420:in `block in check_requirements'
    /usr/local/Homebrew/Library/Homebrew/formula_installer.rb:419:in `each_pair'
    /usr/local/Homebrew/Library/Homebrew/formula_installer.rb:419:in `check_requirements'
    /usr/local/Homebrew/Library/Homebrew/formula_installer.rb:392:in `compute_dependencies'
    /usr/local/Homebrew/Library/Homebrew/formula_installer.rb:149:in `verify_deps_exist'
    /usr/local/Homebrew/Library/Homebrew/formula_installer.rb:143:in `prelude'
    /usr/local/Homebrew/Library/Homebrew/cmd/install.rb:328:in `install_formula'
    /usr/local/Homebrew/Library/Homebrew/cmd/install.rb:261:in `block in install'
    /usr/local/Homebrew/Library/Homebrew/cmd/install.rb:259:in `each'
    /usr/local/Homebrew/Library/Homebrew/cmd/install.rb:259:in `install'
    /usr/local/Homebrew/Library/Homebrew/brew.rb:110:in `<main>'
    ```

--- Library/Homebrew/formula_installer.rb
+++ Library/Homebrew/formula_installer.rb
@@ -418,7 +418,7 @@ class FormulaInstaller
 
     req_map.each_pair do |dependent, reqs|
       reqs.each do |req|
-        next if dependent.installed? && req.name == "maximummacos"
+        next if dependent.latest_version_installed? && req.name == "maximummacos"
 
         @requirement_messages << "#{dependent}: #{req.message}"
         fatals << req if req.fatal?
