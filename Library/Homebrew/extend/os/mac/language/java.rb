d56c6b31a0ce9e28d6b608c41bce6afcce8e71e5
Bo Anderson <mail@boanderson.me>
2020-06-10 15:11:56 +0100

    language/java: deprecate java_home_cmd

--- Library/Homebrew/extend/os/mac/language/java.rb
+++ Library/Homebrew/extend/os/mac/language/java.rb
@@ -2,19 +2,20 @@
 
 module Language
   module Java
-    def self.java_home_cmd(version = nil)
+    def self.system_java_home_cmd(version = nil)
       version_flag = " --version #{version}" if version
       "/usr/libexec/java_home#{version_flag}"
     end
+    private_class_method :system_java_home_cmd
 
     def self.java_home(version = nil)
-      cmd = Language::Java.java_home_cmd(version)
+      cmd = system_java_home_cmd(version)
       Pathname.new Utils.popen_read(cmd).chomp
     end
 
     # @private
     def self.java_home_shell(version = nil)
-      "$(#{java_home_cmd(version)})"
+      "$(#{system_java_home_cmd(version)})"
     end
   end
 end
