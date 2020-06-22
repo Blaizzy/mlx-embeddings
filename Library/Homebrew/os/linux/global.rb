6106bfc4976dff2b03cf3fadbbf92dd1fd9e0f09
rmnull <rmnull@users.noreply.github.com>
2020-06-23 00:09:41 +0530

    Enable patchelf.rb for HOMEBREW_DEVELOPERs.
    HOMEBREW_PATCHELF_RB is enabled on 2 cases.
    1) `HOMEBREW_PATCHELF_RB` is set
    2) `HOMEBREW_DEVELOPER` is set , and `HOMEBREW_NO_PATCHELF_RB` is not set.
    
    use `HOMEBREW_NO_PATCHELF_RB` to turn it off for devs.
    
     Note: When HOMEBREW_PATCHELF_RB and HOMEBREW_NO_PATCHELF_RB both are
        present, it is on

--- Library/Homebrew/os/linux/global.rb
+++ Library/Homebrew/os/linux/global.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 # enables experimental readelf.rb, patchelf support.
-HOMEBREW_PATCHELF_RB = ENV["HOMEBREW_PATCHELF_RB"].present?.freeze
+HOMEBREW_PATCHELF_RB = (ENV["HOMEBREW_PATCHELF_RB"].present? ||
+                        (ENV["HOMEBREW_DEVELOPER"].present? && ENV["HOMEBREW_NO_PATCHELF_RB"].blank?)).freeze
 
 module Homebrew
   DEFAULT_PREFIX ||= if Homebrew::EnvConfig.force_homebrew_on_linux?
