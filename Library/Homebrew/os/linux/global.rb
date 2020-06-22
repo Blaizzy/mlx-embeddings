de6fe2edc1d0bc8facfd932abe5fa4018f197a40
Shaun Jackman <sjackman@gmail.com>
2020-06-22 14:14:33 -0700

    Revert "Enable patchelf.rb for HOMEBREW_DEVELOPERs."
    This reverts commit 6106bfc4976dff2b03cf3fadbbf92dd1fd9e0f09.
    
    Fix the error:
    brew install patchelf
    ==> Pouring patchelf-0.10.x86_64_linux.bottle.1.tar.gz
    Error: cannot load such file -- patchelf

--- Library/Homebrew/os/linux/global.rb
+++ Library/Homebrew/os/linux/global.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 # enables experimental readelf.rb, patchelf support.
-HOMEBREW_PATCHELF_RB = (ENV["HOMEBREW_PATCHELF_RB"].present? ||
-                        (ENV["HOMEBREW_DEVELOPER"].present? && ENV["HOMEBREW_NO_PATCHELF_RB"].blank?)).freeze
+HOMEBREW_PATCHELF_RB = ENV["HOMEBREW_PATCHELF_RB"].present?.freeze
 
 module Homebrew
   DEFAULT_PREFIX ||= if Homebrew::EnvConfig.force_homebrew_on_linux?
