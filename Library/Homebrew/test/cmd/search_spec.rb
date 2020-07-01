7d53104c4680dc24a0f1398e79ce296052ea85e1
Mike McQuaid <mike@mikemcquaid.com>
2020-07-01 11:55:25 +0100

    cmd/search_spec: split Linux/macOS tests.

--- Library/Homebrew/test/cmd/search_spec.rb
+++ Library/Homebrew/test/cmd/search_spec.rb
@@ -8,12 +8,21 @@ describe "Homebrew.search_args" do
 end
 
 describe "brew search", :integration_test do
-  it "falls back to a GitHub tap search when no formula is found", :needs_network do
+  it "falls back to a GitHub tap search when no formula is found", :needs_macos, :needs_network do
     setup_test_formula "testball"
     setup_remote_tap "homebrew/cask"
 
     expect { brew "search", "homebrew/cask/firefox" }
       .to output(/firefox/).to_stdout
+      .and be_a_success
+  end
+
+  # doesn't actually need Linux but only want one integration test per-OS.
+  it "finds formula in search", :need_linux do
+    setup_test_formula "testball"
+
+    expect { brew "search", "testball" }
+      .to output(/testball/).to_stdout
       .and be_a_success
   end
 end
