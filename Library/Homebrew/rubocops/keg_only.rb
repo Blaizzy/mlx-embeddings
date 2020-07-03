e252de5295aa2991c1e0b21af0ce7975766ad742
Rylan Polster <rslpolster@gmail.com>
2020-07-03 14:35:32 -0400

    style: improve keg_only style checks

--- Library/Homebrew/rubocops/keg_only.rb
+++ Library/Homebrew/rubocops/keg_only.rb
@@ -22,18 +22,28 @@ module RuboCop
             Firefox
           ].freeze
 
-          reason = string_content(parameters(keg_only_node).first)
+          reason = parameters(keg_only_node).first
+          offending_node(reason)
           name = Regexp.new(@formula_name, Regexp::IGNORECASE)
-          reason = reason.sub(name, "")
+          reason = string_content(reason).sub(name, "")
           first_word = reason.split.first
 
           if reason =~ /\A[A-Z]/ && !reason.start_with?(*allowlist)
-            problem "'#{first_word}' from the keg_only reason should be '#{first_word.downcase}'."
+            problem "'#{first_word}' from the `keg_only` reason should be '#{first_word.downcase}'."
           end
 
           return unless reason.end_with?(".")
 
-          problem "keg_only reason should not end with a period."
+          problem "`keg_only` reason should not end with a period."
+        end
+
+        def autocorrect(node)
+          lambda do |corrector|
+            reason = string_content(node)
+            reason[0] = reason[0].downcase
+            reason = reason.delete_suffix(".")
+            corrector.replace(node.source_range, "\"#{reason}\"")
+          end
         end
       end
     end
