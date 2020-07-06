d7149d1f76621ffb301c62292c09be8dbda9566f
Baffour Adu Boampong <baffouraduboampong@gmail.com>
2020-07-06 03:32:18 +0000

    Fix merge conflicts

--- Library/Homebrew/dev-cmd/bump.rb
+++ Library/Homebrew/dev-cmd/bump.rb
@@ -12,16 +12,92 @@ module Homebrew
 
         Display out-of-date brew formulae, the latest version available, and whether a pull request has been opened.
       EOS
+      switch :verbose
+      switch :debug
     end
   end
 
   def bump
     bump_args.parse
 
-    outdated_repology_packages = RepologyParser.parse_api_response
-    outdated_packages = RepologyParser.validate_and_format_packages(outdated_repology_packages)
+    outdated_repology_packages = Repology.parse_api_response
+    outdated_packages = validate_and_format_packages(outdated_repology_packages)
 
     display(outdated_packages)
+  end
+
+  def validate_and_format_packages(outdated_repology_packages)
+    ohai "Verifying outdated repology packages as Homebrew Formulae"
+
+    packages = {}
+    outdated_repology_packages.each do |_name, repositories|
+      # identify homebrew repo
+      repology_homebrew_repo = repositories.find do |repo|
+        repo["repo"] == "homebrew"
+      end
+
+      next if repology_homebrew_repo.empty?
+
+      latest_version = nil
+
+      # identify latest version amongst repology repos
+      repositories.each do |repo|
+        latest_version = repo["version"] if repo["status"] == "newest"
+      end
+
+      packages[repology_homebrew_repo["srcname"]] = format_package(repology_homebrew_repo["srcname"], latest_version)
+    end
+    packages
+  end
+
+  def format_package(package_name, latest_version)
+    formula = get_formula_details(package_name)
+    return if formula.nil?
+
+    tap_full_name = formula.tap&.full_name
+    current_version = current_formula_version(formula)
+    livecheck_response = livecheck_formula(package_name)
+    pull_requests = GitHub.check_for_duplicate_pull_requests(formula, tap_full_name, latest_version, args, true)
+
+    {
+      repology_latest_version:  latest_version,
+      current_formula_version:  current_version.to_s,
+      livecheck_latest_version: livecheck_response[:livecheck_version],
+      open_pull_requests:       pull_requests,
+    }
+  end
+
+  def get_formula_details(formula_name)
+    Formula[formula_name]
+  rescue
+    nil
+  end
+
+  def current_formula_version(formula)
+    formula.version.to_s
+  rescue
+    nil
+  end
+
+  def livecheck_formula(formula)
+    ohai "Checking livecheck formula: #{formula}" if Homebrew.args.verbose?
+
+    response = Utils.popen_read(HOMEBREW_BREW_FILE, "livecheck", formula, "--quiet").chomp
+
+    parse_livecheck_response(response)
+  end
+
+  def parse_livecheck_response(response)
+    output = response.delete(" ").split(/:|==>/)
+
+    # eg: ["openclonk", "7.0", "8.1"]
+    package_name, brew_version, latest_version = output
+
+    {
+      name:              package_name,
+      formula_version:   brew_version,
+      livecheck_version: latest_version,
+    }
   end
 
   def display(outdated_packages)
--- Library/Homebrew/utils/repology.rb
+++ Library/Homebrew/utils/repology.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 require "utils/curl"
-require "utils/versions"
 
-require "formula_info"
-
-module RepologyParser
+module Repology
   module_function
 
   MAX_PAGINATION = 15
@@ -14,7 +11,6 @@ module RepologyParser
     last_package_in_response += "/" unless last_package_in_response.empty?
 
     url = "https://repology.org/api/v1/projects/#{last_package_in_response}?inrepo=homebrew&outdated=1"
-    ohai "Calling API #{url}" if Homebrew.args.verbose?
 
     output, _errors, _status = curl_output(url.to_s)
     JSON.parse(output)
@@ -29,7 +25,7 @@ module RepologyParser
     page_no = 1
 
     while response_size > 1 && page_no <= MAX_PAGINATION
-      ohai "Paginating Repology api page: #{page_no}" if Homebrew.args.verbose?
+      ohai "Paginating Repology api page: #{page_no}" if Homebrew.args.debug?
 
       last_package_in_response = outdated_packages.keys[last_package_index]
       response = query_api(last_package_in_response)
@@ -43,42 +39,5 @@ module RepologyParser
     ohai "#{outdated_packages.size} outdated packages identified"
 
     outdated_packages
-  end
-
-  def validate_and_format_packages(outdated_repology_packages)
-    ohai "Verifying outdated repology packages as Homebrew Formulae"
-
-    packages = {}
-    outdated_repology_packages.each do |_name, repositories|
-      # identify homebrew repo
-      repology_homebrew_repo = repositories.find do |repo|
-        repo["repo"] == "homebrew"
-      end
-
-      next if repology_homebrew_repo.empty?
-
-      latest_version = nil
-
-      # identify latest version amongst repology repos
-      repositories.each do |repo|
-        latest_version = repo["version"] if repo["status"] == "newest"
-      end
-
-      packages[repology_homebrew_repo["srcname"]] = format_package(repology_homebrew_repo["srcname"], latest_version)
-    end
-    packages
-  end
-
-  def format_package(package_name, latest_version)
-    current_version = Versions.current_formula_version(package_name)
-    livecheck_response = Versions.livecheck_formula(package_name)
-    pull_requests = Versions.check_for_duplicate_pull_requests(package_name, latest_version)
-
-    {
-      repology_latest_version:  latest_version,
-      current_formula_version:  current_version.to_s,
-      livecheck_latest_version: livecheck_response[:livecheck_version],
-      open_pull_requests:       pull_requests,
-    }
   end
 end
