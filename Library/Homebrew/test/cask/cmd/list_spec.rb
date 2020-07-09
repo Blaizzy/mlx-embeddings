832073a3c7de32346d09409569b48ed672418527
Sean Molenaar <smillerdev@me.com>
2020-07-09 22:58:48 +0200

    cask: add list --json

--- Library/Homebrew/test/cask/cmd/list_spec.rb
+++ Library/Homebrew/test/cask/cmd/list_spec.rb
@@ -20,6 +20,26 @@ describe Cask::Cmd::List, :cask do
     EOS
   end
 
+  it "lists oneline" do
+    casks = %w[
+      local-caffeine
+      third-party/tap/third-party-cask
+      local-transmission
+    ].map { |c| Cask::CaskLoader.load(c) }
+
+    casks.each do |c|
+      InstallHelper.install_with_caskfile(c)
+    end
+
+    expect {
+      described_class.run("-1")
+    }.to output(<<~EOS).to_stdout
+      local-caffeine
+      local-transmission
+      third-party-cask
+    EOS
+  end
+
   it "lists full names" do
     casks = %w[
       local-caffeine
@@ -62,6 +82,31 @@ describe Cask::Cmd::List, :cask do
     it "of given Casks" do
       expect {
         described_class.run("--versions", "local-caffeine", "local-transmission")
+      }.to output(expected_output).to_stdout
+    end
+  end
+
+  describe "lists json" do
+    let(:casks) { ["local-caffeine", "local-transmission"] }
+    let(:expected_output) {
+      <<~EOS
+        [{"token":"local-caffeine","name":[],"homepage":"https://brew.sh/","url":"file:///usr/local/Homebrew/Library/Homebrew/test/support/fixtures/cask/caffeine.zip","appcast":null,"version":"1.2.3","sha256":"67cdb8a02803ef37fdbf7e0be205863172e41a561ca446cd84f0d7ab35a99d94","artifacts":[["Caffeine.app"]],"caveats":null,"depends_on":{},"conflicts_with":null,"container":null,"auto_updates":null},{"token":"local-transmission","name":["Transmission"],"homepage":"https://brew.sh/","url":"file:///usr/local/Homebrew/Library/Homebrew/test/support/fixtures/cask/transmission-2.61.dmg","appcast":null,"version":"2.61","sha256":"e44ffa103fbf83f55c8d0b1bea309a43b2880798dae8620b1ee8da5e1095ec68","artifacts":[["Transmission.app"]],"caveats":null,"depends_on":{},"conflicts_with":null,"container":null,"auto_updates":null}]
+      EOS
+    }
+
+    before do
+      casks.map(&Cask::CaskLoader.method(:load)).each(&InstallHelper.method(:install_with_caskfile))
+    end
+
+    it "of all installed Casks" do
+      expect {
+        described_class.run("--json")
+      }.to output(expected_output).to_stdout
+    end
+
+    it "of given Casks" do
+      expect {
+        described_class.run("--json", "local-caffeine", "local-transmission")
       }.to output(expected_output).to_stdout
     end
   end
