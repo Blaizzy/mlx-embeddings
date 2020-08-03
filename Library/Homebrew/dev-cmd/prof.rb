10f2a5025621aed308122880d0796e8a7b713bb9
Bo Anderson <mail@boanderson.me>
2020-08-03 02:07:06 +0100

    dev-cmd/prof: improve arg passing

--- Library/Homebrew/dev-cmd/prof.rb
+++ Library/Homebrew/dev-cmd/prof.rb
@@ -8,20 +8,19 @@ module Homebrew
   def prof_args
     Homebrew::CLI::Parser.new do
       usage_banner <<~EOS
-        `prof` <command>
+        `prof` [<command>]
 
         Run Homebrew with the Ruby profiler, e.g. `brew prof readall`.
       EOS
-      min_named 1
     end
   end
 
   def prof
-    prof_args.parse
+    args = prof_args.parse
 
     Homebrew.install_gem_setup_path! "ruby-prof", version: "0.18.0"
     FileUtils.mkdir_p "prof"
     brew_rb = (HOMEBREW_LIBRARY_PATH/"brew.rb").resolved_path
-    safe_system "ruby-prof", "--printer=multi", "--file=prof", brew_rb, "--", *ARGV
+    safe_system "ruby-prof", "--printer=multi", "--file=prof", brew_rb, "--", *args.named
   end
 end
