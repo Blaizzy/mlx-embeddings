62c2ee332f3b37541682f39dd200997ce376a417
Markus Reiter <me@reitermark.us>
2020-08-09 03:14:44 +0200

    Document `LazyObject`.

--- Library/Homebrew/lazy_object.rb
+++ Library/Homebrew/lazy_object.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# An object which lazily evaluates its inner block only once a method is called on it.
+#
+# @api private
 class LazyObject < Delegator
   def initialize(&callable)
     super(callable)
