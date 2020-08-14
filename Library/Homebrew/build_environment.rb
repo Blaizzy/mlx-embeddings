b5363025a675d0b8f661333a87b31a817a79301d
Markus Reiter <me@reitermark.us>
2020-08-14 02:26:29 +0200

    Refactor and document `BuildEnvironment`.

--- Library/Homebrew/build_environment.rb
+++ Library/Homebrew/build_environment.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Settings for the build environment.
+#
+# @api private
 class BuildEnvironment
   def initialize(*settings)
     @settings = Set.new(*settings)
@@ -23,35 +26,35 @@ class BuildEnvironment
     @settings.include? :userpaths
   end
 
+  # DSL for specifying build environment settings.
   module DSL
     def env(*settings)
       @env ||= BuildEnvironment.new
       @env.merge(settings)
     end
   end
-end
 
-module Homebrew
-  module_function
+  KEYS = %w[
+    CC CXX LD OBJC OBJCXX
+    HOMEBREW_CC HOMEBREW_CXX
+    CFLAGS CXXFLAGS CPPFLAGS LDFLAGS SDKROOT MAKEFLAGS
+    CMAKE_PREFIX_PATH CMAKE_INCLUDE_PATH CMAKE_LIBRARY_PATH CMAKE_FRAMEWORK_PATH
+    MACOSX_DEPLOYMENT_TARGET PKG_CONFIG_PATH PKG_CONFIG_LIBDIR
+    HOMEBREW_DEBUG HOMEBREW_MAKE_JOBS HOMEBREW_VERBOSE
+    HOMEBREW_SVN HOMEBREW_GIT
+    HOMEBREW_SDKROOT
+    MAKE GIT CPP
+    ACLOCAL_PATH PATH CPATH
+    LD_LIBRARY_PATH LD_RUN_PATH LD_PRELOAD LIBRARY_PATH
+  ].freeze
+  private_constant :KEYS
 
-  def build_env_keys(env)
-    %w[
-      CC CXX LD OBJC OBJCXX
-      HOMEBREW_CC HOMEBREW_CXX
-      CFLAGS CXXFLAGS CPPFLAGS LDFLAGS SDKROOT MAKEFLAGS
-      CMAKE_PREFIX_PATH CMAKE_INCLUDE_PATH CMAKE_LIBRARY_PATH CMAKE_FRAMEWORK_PATH
-      MACOSX_DEPLOYMENT_TARGET PKG_CONFIG_PATH PKG_CONFIG_LIBDIR
-      HOMEBREW_DEBUG HOMEBREW_MAKE_JOBS HOMEBREW_VERBOSE
-      HOMEBREW_SVN HOMEBREW_GIT
-      HOMEBREW_SDKROOT
-      MAKE GIT CPP
-      ACLOCAL_PATH PATH CPATH
-      LD_LIBRARY_PATH LD_RUN_PATH LD_PRELOAD LIBRARY_PATH
-    ].select { |key| env.key?(key) }
+  def self.keys(env)
+    KEYS & env.keys
   end
 
-  def dump_build_env(env, f = $stdout)
-    keys = build_env_keys(env)
+  def self.dump(env, f = $stdout)
+    keys = self.keys(env)
     keys -= %w[CC CXX OBJC OBJCXX] if env["CC"] == env["HOMEBREW_CC"]
 
     keys.each do |key|
