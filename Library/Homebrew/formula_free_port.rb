b6b102effbfe6abf5dfac4dfcc5643bd4784d775
Markus Reiter <me@reitermark.us>
2020-08-17 06:01:54 +0200

    Document `FreePort`.

--- Library/Homebrew/formula_free_port.rb
+++ Library/Homebrew/formula_free_port.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
-module Homebrew
-  module FreePort
-    require "socket"
+require "socket"
 
+module Homebrew
+  # Helper function for finding a free port.
+  #
+  # @api private
+  module FreePort
+    # Returns a free port.
+    # @api public
     def free_port
       server = TCPServer.new 0
       _, port, = server.addr
