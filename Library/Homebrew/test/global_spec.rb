281632b64ec52e1052bf3d09a280a376bfa75906
Bo Anderson <mail@boanderson.me>
2020-08-18 16:47:20 +0100

    test: ensure `require "formula"` isn't run on startup

---
+++ Library/Homebrew/test/global_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+describe "brew", :integration_test do
+  it "does not invoke `require \"formula\"` at startup" do
+    expect { brew "verify-formula-undefined" }
+      .to not_to_output.to_stdout
+      .and not_to_output.to_stderr
+      .and be_a_success
+  end
+end
