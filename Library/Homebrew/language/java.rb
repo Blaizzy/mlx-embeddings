d5f552f3a02bbc63883b1c2155098cce317dd334
Seeker <meaningseeking@protonmail.com>
2020-08-21 09:58:50 -0700

    language/java: check installed openjdk version instead of latest version

--- Library/Homebrew/language/java.rb
+++ Library/Homebrew/language/java.rb
@@ -11,7 +11,7 @@ module Language
         next false unless f.any_version_installed?
 
         unless version.zero?
-          major = f.version.to_s[/\d+/].to_i
+          major = f.opt_or_installed_prefix_keg.version.major
           next false if major < version
           next false if major > version && !can_be_newer
         end
