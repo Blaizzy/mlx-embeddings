117ae2c068e5795b08c15027ed89e7d1ddd004eb
Markus Reiter <me@reitermark.us>
2020-10-09 18:00:30 +0200

    Remove redundant check for `sorbet-runtime`.

--- Library/Homebrew/utils/sorbet.rb
+++ Library/Homebrew/utils/sorbet.rb
@@ -6,12 +6,5 @@ if ENV["HOMEBREW_SORBET_RUNTIME"]
   Homebrew.install_bundler_gems!
   require "sorbet-runtime"
 else
-  begin
-    gem "sorbet-runtime"
-    raise "Loaded `sorbet-runtime` instead of `sorbet-runtime-stub`."
-  rescue Gem::LoadError
-    nil
-  end
-
   require "sorbet-runtime-stub"
 end
