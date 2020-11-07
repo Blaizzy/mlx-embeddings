5c6a12e5b77c4aeedd54df8d5449d432fb130025
EricFromCanada <enk3@outlook.com>
2020-11-07 11:00:41 -0500

    requirements: allow `depends_on arch: :arm64`

---
+++ Library/Homebrew/test/requirements/arch_requirement_spec.rb
@@ -0,0 +1,14 @@
+# typed: false
+# frozen_string_literal: true
+
+require "requirements/arch_requirement"
+
+describe ArchRequirement do
+  subject(:requirement) { described_class.new([Hardware::CPU.type]) }
+
+  describe "#satisfied?" do
+    it "supports architecture symbols" do
+      expect(requirement).to be_satisfied
+    end
+  end
+end
