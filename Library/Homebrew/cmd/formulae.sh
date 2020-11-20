2a2971504273d2ab9e42d17ab77bf30e7f04acba
Mike McQuaid <mike@mikemcquaid.com>
2020-11-20 12:43:38 +0000

    cmd/formulae.sh: improve and refactor.

--- Library/Homebrew/cmd/formulae.sh
+++ Library/Homebrew/cmd/formulae.sh
@@ -5,9 +5,24 @@
 
 homebrew-formulae() {
   local formulae
-  formulae="$(find "$HOMEBREW_REPOSITORY"/Library/Taps -type d \( -name Casks -o -name cmd -o -name .github \) -prune -false -o -name '*rb' |\
-  sed -E -e 's/\.rb//g' -e 's_.*/Taps/(.*)/(home|linux)brew-_\1/_' -e 's|/Formula/|/|')"
+  formulae="$( \
+    find "$HOMEBREW_REPOSITORY/Library/Taps" \
+         -type d \( \
+           -name Casks -o \
+           -name cmd -o \
+           -name .github -o \
+           -name lib -o \
+           -name spec -o \
+           -name vendor \
+          \) \
+         -prune -false -o -name '*\.rb' | \
+    sed -E -e 's/\.rb//g' \
+           -e 's_.*/Taps/(.*)/(home|linux)brew-_\1/_' \
+           -e 's|/Formula/|/|' \
+  )"
   local shortnames
-  shortnames="$(echo "$formulae" | cut -d / -f 3)"
-  echo -e "$formulae \n $shortnames" | grep -v '^homebrew/' | sort -uf
+  shortnames="$(echo "$formulae" | cut -d "/" -f 3)"
+  echo -e "$formulae \n $shortnames" | \
+    grep -v '^homebrew/core' | \
+    sort -uf
 }
