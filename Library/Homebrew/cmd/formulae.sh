917b4b964fc210446feeed87cb90577a804588c6
metacollin <metacollin@electropi.mp>
2020-11-19 20:33:18 -0700

    Declare local variables seperately, split up line for readability.

--- Library/Homebrew/cmd/formulae.sh
+++ Library/Homebrew/cmd/formulae.sh
@@ -3,9 +3,11 @@
 #:  List all locally installable formulae including short names.
 #:
 
-# shellcheck disable=SC2155
 homebrew-formulae() {
-  local formulae="$(\find "$HOMEBREW_REPOSITORY"/Library/Taps -type d \( -name Casks -o -name cmd -o -name .github \) -prune -false -o -name '*rb' | \sed -E -e 's/\.rb//g' -e 's_.*/Taps/(.*)/(home|linux)brew-_\1/_' -e 's|/Formula/|/|')"
-  local shortnames="$(echo "$formulae" | \cut -d / -f 3)"
+  local formulae
+  formulae="$(\find "$HOMEBREW_REPOSITORY"/Library/Taps -type d \( -name Casks -o -name cmd -o -name .github \) -prune -false -o -name '*rb' |\
+  \sed -E -e 's/\.rb//g' -e 's_.*/Taps/(.*)/(home|linux)brew-_\1/_' -e 's|/Formula/|/|')"
+  local shortnames
+  shortnames="$(echo "$formulae" | \cut -d / -f 3)"
   echo -e "$formulae \n $shortnames" | \grep -v '^homebrew/' | \sort -uf
 }
