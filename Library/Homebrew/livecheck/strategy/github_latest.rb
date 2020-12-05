28bfa0c93f4400e714c2ef54bb290e3e17baf6d2
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-05 11:53:50 -0500

    GithubLatest: Rework documentation comments

--- Library/Homebrew/livecheck/strategy/github_latest.rb
+++ Library/Homebrew/livecheck/strategy/github_latest.rb
@@ -5,7 +5,7 @@ module Homebrew
   module Livecheck
     module Strategy
       # The {GithubLatest} strategy identifies versions of software at
-      # github.com by checking a repository's latest release page.
+      # github.com by checking a repository's "latest" release page.
       #
       # GitHub URLs take a few different formats:
       #
@@ -13,17 +13,30 @@ module Homebrew
       # * `https://github.com/example/example/archive/v1.2.3.tar.gz`
       # * `https://github.com/downloads/example/example/example-1.2.3.tar.gz`
       #
-      # This strategy is used when latest releases are marked for software hosted
-      # on GitHub. It is necessary to use `strategy :github_latest` in a `livecheck`
-      # block for Livecheck to use this strategy.
+      # A repository's `/releases/latest` URL normally redirects to a release
+      # tag (e.g., `/releases/tag/1.2.3`). When there isn't a "latest" release,
+      # it will redirect to the `/releases` page.
       #
-      # The default regex identifies versions from `href` attributes containing the
-      # tag name.
+      # This strategy should only be used when we know the upstream repository
+      # has a "latest" release and the tagged release is appropriate to use
+      # (e.g., "latest" isn't wrongly pointing to an unstable version, not
+      # picking up the actual latest version, etc.). The strategy can only be
+      # applied by using `strategy :github_latest` in a `livecheck` block.
+      #
+      # The default regex identifies versions like `1.2.3`/`v1.2.3` in `href`
+      # attributes containing the tag URL (e.g.,
+      # `/example/example/releases/tag/v1.2.3`). This is a common tag format
+      # but a modified regex can be provided in a `livecheck` block to override
+      # the default if a repository uses a different format (e.g.,
+      # `example-1.2.3`, `1.2.3d`, `1.2.3-4`, etc.).
       #
       # @api public
       class GithubLatest
         NICE_NAME = "GitHub - Latest"
 
+        # A priority of zero causes livecheck to skip the strategy. We do this
+        # for {GithubLatest} so we can selectively apply the strategy using
+        # `strategy :github_latest` in a `livecheck` block.
         PRIORITY = 0
 
         # The `Regexp` used to determine if the strategy applies to the URL.
@@ -46,12 +59,10 @@ module Homebrew
         def self.find_versions(url, regex = nil)
           %r{github\.com/(?:downloads/)?(?<username>[^/]+)/(?<repository>[^/]+)}i =~ url.sub(/\.git$/i, "")
 
-          # The page containing the latest release
+          # Example URL: `https://github.com/example/example/releases/latest`
           page_url = "https://github.com/#{username}/#{repository}/releases/latest"
 
-          # The default regex applies to most repositories, but may have to be
-          # replaced with a specific regex when the tag names contain the package
-          # name or other characters apart from the version.
+          # The default regex is the same for all URLs using this strategy
           regex ||= %r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i
 
           Homebrew::Livecheck::Strategy::PageMatch.find_versions(page_url, regex)
