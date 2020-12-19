aaf7bc2bc5533f2ab177374193ad260f97b79985
Seeker <meaningseeking@protonmail.com>
2020-12-18 17:24:10 -0800

    utils: add AST helper functions for editing formulae

--- Library/Homebrew/utils/bottles.rb
+++ Library/Homebrew/utils/bottles.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 require "tab"
+require "utils/ast"
 
 module Utils
   # Helper functions for bottles.
@@ -76,75 +77,10 @@ module Utils
       end
 
       def add_bottle_stanza!(formula_contents, bottle_output)
-        Homebrew.install_bundler_gems!
-        require "rubocop-ast"
-
-        ruby_version = Version.new(HOMEBREW_REQUIRED_RUBY_VERSION).major_minor.to_f
-        processed_source = RuboCop::AST::ProcessedSource.new(formula_contents, ruby_version)
-        root_node = processed_source.ast
-
-        class_node = if root_node.class_type?
-          root_node
-        elsif root_node.begin_type?
-          root_node.children.find do |n|
-            n.class_type? && n.parent_class&.const_name == "Formula"
-          end
-        end
-
-        odie "Could not find formula class!" if class_node.nil?
-
-        body_node = class_node.body
-        odie "Formula class is empty!" if body_node.nil?
-
-        node_before_bottle = if body_node.begin_type?
-          body_node.children.compact.reduce do |previous_child, current_child|
-            break previous_child unless component_before_bottle_block? current_child
-
-            current_child
-          end
-        else
-          body_node
-        end
-        node_before_bottle = node_before_bottle.last_argument if node_before_bottle.send_type?
-
-        expr_before_bottle = node_before_bottle.location.expression
-        processed_source.comments.each do |comment|
-          comment_expr = comment.location.expression
-          distance = comment_expr.first_line - expr_before_bottle.first_line
-          case distance
-          when 0
-            if comment_expr.last_line > expr_before_bottle.last_line ||
-               comment_expr.end_pos > expr_before_bottle.end_pos
-              expr_before_bottle = comment_expr
-            end
-          when 1
-            expr_before_bottle = comment_expr
-          end
-        end
-
-        tree_rewriter = Parser::Source::TreeRewriter.new(processed_source.buffer)
-        tree_rewriter.insert_after(expr_before_bottle, "\n\n#{bottle_output.chomp}")
-        formula_contents.replace(tree_rewriter.process)
-      end
-
-      private
-
-      def component_before_bottle_block?(node)
-        require "rubocops/components_order"
-
-        RuboCop::Cop::FormulaAudit::ComponentsOrder::COMPONENT_PRECEDENCE_LIST.each do |components|
-          components.each do |component|
-            return false if component[:name] == :bottle && component[:type] == :block_call
-
-            case component[:type]
-            when :method_call
-              return true if node.send_type? && node.method_name == component[:name]
-            when :block_call
-              return true if node.block_type? && node.method_name == component[:name]
-            end
-          end
-        end
-        false
+        Utils::AST.add_formula_stanza!(formula_contents,
+                                       name: :bottle,
+                                       type: :block_call,
+                                       text: "\n#{bottle_output.chomp}")
       end
     end
 
