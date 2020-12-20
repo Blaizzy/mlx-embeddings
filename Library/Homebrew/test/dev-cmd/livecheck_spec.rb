c7080ba812c707aafdd23f79211f7d11f484e015
Sam Ford <1584702+samford@users.noreply.github.com>
2020-12-19 23:02:42 -0500

    Expand dev-cmd/livecheck tests

--- Library/Homebrew/test/dev-cmd/livecheck_spec.rb
+++ Library/Homebrew/test/dev-cmd/livecheck_spec.rb
@@ -21,4 +21,11 @@ describe "brew livecheck", :integration_test do
       .and not_to_output.to_stderr
       .and be_a_success
   end
+
+  it "gives an error when no arguments are given and there's no watchlist" do
+    expect { brew "livecheck" }
+      .to output(/Invalid usage: A watchlist file is required when no arguments are given\./).to_stderr
+      .and not_to_output.to_stdout
+      .and be_a_failure
+  end
 end
