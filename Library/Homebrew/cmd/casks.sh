98c968ea2f5e9239431dcf4ce5a52793fff77ee0
Kid <kidonng@gmail.com>
2020-12-25 22:32:48 +0800

    Add brew casks command

---
+++ Library/Homebrew/cmd/casks.sh
@@ -0,0 +1,29 @@
+#:  * `casks`
+#:
+#:  List all locally installable casks including short names.
+#:
+
+homebrew-casks() {
+  local casks
+  local sed_extended_regex_flag
+
+  if [[ -n "$HOMEBREW_MACOS" ]]; then
+    sed_extended_regex_flag="-E"
+  else
+    sed_extended_regex_flag="-r"
+  fi
+
+  casks="$( \
+    find "$HOMEBREW_REPOSITORY/Library/Taps" \
+         -maxdepth 4 -path '*/Casks/*.rb' | \
+    sed "$sed_extended_regex_flag" \
+      -e 's/\.rb//g' \
+      -e 's_.*/Taps/(.*)/(home|linux)brew-_\1/_' \
+      -e 's|/Casks/|/|' \
+  )"
+  local shortnames
+  shortnames="$(echo "$casks" | cut -d "/" -f 3)"
+  echo -e "$casks\n$shortnames" | \
+    grep -v '^homebrew/cask' | \
+    sort -uf
+}
--- completions/fish/brew.fish
+++ completions/fish/brew.fish
@@ -206,7 +206,7 @@ function __fish_brew_suggest_casks_outdated -d "Lists outdated casks with the in
 end
 
 function __fish_brew_suggest_casks_all -d "Lists locally available casks"
-    brew search --cask
+    brew casks
 end
 
 
