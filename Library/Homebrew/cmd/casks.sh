9b9c0c8626025a9d108b8d3707afbafe212f288e
Kid <kidonng@gmail.com>
2021-01-08 18:04:51 +0800

    Add homebrew-items

--- Library/Homebrew/cmd/casks.sh
+++ Library/Homebrew/cmd/casks.sh
@@ -3,27 +3,8 @@
 #:  List all locally installable casks including short names.
 #:
 
+source "$HOMEBREW_LIBRARY/Homebrew/items.sh"
+
 homebrew-casks() {
-  local casks
-  local sed_extended_regex_flag
-
-  if [[ -n "$HOMEBREW_MACOS" ]]; then
-    sed_extended_regex_flag="-E"
-  else
-    sed_extended_regex_flag="-r"
-  fi
-
-  casks="$( \
-    find "$HOMEBREW_REPOSITORY/Library/Taps" \
-         -maxdepth 4 -path '*/Casks/*.rb' | \
-    sed "$sed_extended_regex_flag" \
-      -e 's/\.rb//g' \
-      -e 's_.*/Taps/(.*)/(home|linux)brew-_\1/_' \
-      -e 's|/Casks/|/|' \
-  )"
-  local shortnames
-  shortnames="$(echo "$casks" | cut -d "/" -f 3)"
-  echo -e "$casks\n$shortnames" | \
-    grep -v '^homebrew/cask' | \
-    sort -uf
+  homebrew-items 'Formula' 's|/Casks/|/|' '^homebrew/cask'
 }
