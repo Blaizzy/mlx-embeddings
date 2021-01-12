23b8d0ccb8af07d38a67e9befbcbf5058f2e1997
Jonathan Chang <me@jonathanchang.org>
2021-01-12 18:12:56 +1100

    rubocops/desc: use rubocop v1 API

--- Library/Homebrew/rubocops/formula_desc.rb
+++ Library/Homebrew/rubocops/formula_desc.rb
@@ -12,14 +12,12 @@ module RuboCop
       # See the {DescHelper} module for details of the checks.
       class Desc < FormulaCop
         include DescHelper
+        extend AutoCorrector
 
         def audit_formula(_node, _class_node, _parent_class_node, body_node)
+          @name = @formula_name
           desc_call = find_node_method_by_name(body_node, :desc)
-          audit_desc(:formula, @formula_name, desc_call)
-        end
-
-        def autocorrect(node)
-          autocorrect_desc(node, @formula_name)
+          audit_desc(:formula, @name, desc_call)
         end
       end
     end
