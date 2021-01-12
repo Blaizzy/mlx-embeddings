a778dc3bdbc6e182e9ab7be733bcf08793d78853
Jonathan Chang <me@jonathanchang.org>
2021-01-12 13:33:23 +1100

    rubocops/class: use rubocop v1 API

--- Library/Homebrew/rubocops/class.rb
+++ Library/Homebrew/rubocops/class.rb
@@ -10,6 +10,8 @@ module RuboCop
       #
       # @api private
       class ClassName < FormulaCop
+        extend AutoCorrector
+
         DEPRECATED_CLASSES = %w[
           GithubGistFormula
           ScriptFileFormula
@@ -20,12 +22,8 @@ module RuboCop
           parent_class = class_name(parent_class_node)
           return unless DEPRECATED_CLASSES.include?(parent_class)
 
-          problem "#{parent_class} is deprecated, use Formula instead"
-        end
-
-        def autocorrect(node)
-          lambda do |corrector|
-            corrector.replace(node.source_range, "Formula")
+          problem "#{parent_class} is deprecated, use Formula instead" do |corrector|
+            corrector.replace(parent_class_node.source_range, "Formula")
           end
         end
       end
@@ -34,6 +32,8 @@ module RuboCop
       #
       # @api private
       class Test < FormulaCop
+        extend AutoCorrector
+
         def audit_formula(_node, _class_node, _parent_class_node, body_node)
           test = find_block(body_node, :test)
           return unless test
@@ -49,31 +49,17 @@ module RuboCop
             p1, p2 = params
             if match = string_content(p1).match(%r{(/usr/local/(s?bin))})
               offending_node(p1)
-              problem "use \#{#{match[2]}} instead of #{match[1]} in #{node}"
+              problem "use \#{#{match[2]}} instead of #{match[1]} in #{node}" do |corrector|
+                corrector.replace(p1.source_range, p1.source.sub(match[1], "\#{#{match[2]}}"))
+              end
             end
 
             if node == :shell_output && node_equals?(p2, 0)
               offending_node(p2)
-              problem "Passing 0 to shell_output() is redundant"
-            end
-          end
-        end
-
-        def autocorrect(node)
-          lambda do |corrector|
-            case node.type
-            when :str, :dstr
-              # Rubocop: intentionally outputted non-interpolated strings
-              corrector.replace(node.source_range,
-                                node.source.to_s.sub(%r{(/usr/local/(s?bin))},
-                                                     '#{\2}')) # rubocop:disable Lint/InterpolationCheck
-            when :int
-              corrector.remove(
-                range_with_surrounding_comma(
-                  range_with_surrounding_space(range: node.source_range,
-                                               side:  :left),
-                ),
-              )
+              problem "Passing 0 to shell_output() is redundant" do |corrector|
+                corrector.remove(range_with_surrounding_comma(range_with_surrounding_space(range: p2.source_range,
+                                                                                           side:  :left)))
+              end
             end
           end
         end
