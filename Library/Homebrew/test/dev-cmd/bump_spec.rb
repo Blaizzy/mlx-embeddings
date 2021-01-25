9c914106ed9adc47a649a77332cdda57ab89fbbb
nandahkrishna <nanda.harishankar@gmail.com>
2021-01-25 17:12:43 +0530

    dev-cmd/bump: increase test coverage

--- Library/Homebrew/test/dev-cmd/bump_spec.rb
+++ Library/Homebrew/test/dev-cmd/bump_spec.rb
@@ -27,5 +27,19 @@ describe "brew bump" do
         .and not_to_output.to_stderr
         .and be_a_success
     end
+
+    it "returns no data and prints a message for HEAD-only formulae" do
+      content = <<~RUBY
+        desc "HEAD-only test formula"
+        homepage "https://brew.sh"
+        head "https://github.com/Homebrew/brew.git"
+      RUBY
+      setup_test_formula("headonly", content)
+
+      expect { brew "bump", "headonly" }
+        .to output(/Formula is HEAD-only./).to_stdout
+        .and not_to_output.to_stderr
+        .and be_a_success
+    end
   end
 end
