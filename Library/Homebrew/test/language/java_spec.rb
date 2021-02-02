234c1f14b81ebc0685ccad7bff94b457cc078d51
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2021-02-02 09:29:43 +0000

    java_spec: fix tests
    The tests need to match the changes reverted in d6bba1c4938ea9c9a9d58ba2f209775dd71a8537.

--- Library/Homebrew/test/language/java_spec.rb
+++ Library/Homebrew/test/language/java_spec.rb
@@ -18,12 +18,22 @@ describe Language::Java do
   end
 
   describe "::java_home" do
-    it "returns valid JAVA_HOME if version is specified" do
+    it "returns valid JAVA_HOME if version is specified", :needs_macos do
+      java_home = described_class.java_home("1.8+")
+      expect(java_home).to eql(f.opt_libexec/"openjdk.jdk/Contents/Home")
+    end
+
+    it "returns valid JAVA_HOME if version is not specified", :needs_macos do
+      java_home = described_class.java_home
+      expect(java_home).to eql(f.opt_libexec/"openjdk.jdk/Contents/Home")
+    end
+
+    it "returns valid JAVA_HOME if version is specified", :needs_linux do
       java_home = described_class.java_home("1.8+")
       expect(java_home).to eql(f.opt_libexec)
     end
 
-    it "returns valid JAVA_HOME if version is not specified" do
+    it "returns valid JAVA_HOME if version is not specified", :needs_linux do
       java_home = described_class.java_home
       expect(java_home).to eql(f.opt_libexec)
     end
@@ -32,24 +42,24 @@ describe Language::Java do
   describe "::java_home_env" do
     it "returns java_home path if version specified" do
       java_home_env = described_class.java_home_env("1.8+")
-      expect(java_home_env[:JAVA_HOME]).to eql(f.opt_libexec.to_s)
+      expect(java_home_env[:JAVA_HOME]).to include(f.opt_libexec.to_s)
     end
 
     it "returns java_home path if version is not specified" do
       java_home_env = described_class.java_home_env
-      expect(java_home_env[:JAVA_HOME]).to eql(f.opt_libexec.to_s)
+      expect(java_home_env[:JAVA_HOME]).to include(f.opt_libexec.to_s)
     end
   end
 
   describe "::overridable_java_home_env" do
     it "returns java_home path if version specified" do
       overridable_java_home_env = described_class.overridable_java_home_env("1.8+")
-      expect(overridable_java_home_env[:JAVA_HOME]).to eql("${JAVA_HOME:-#{f.opt_libexec}}")
+      expect(overridable_java_home_env[:JAVA_HOME]).to include(f.opt_libexec.to_s)
     end
 
     it "returns java_home path if version is not specified" do
       overridable_java_home_env = described_class.overridable_java_home_env
-      expect(overridable_java_home_env[:JAVA_HOME]).to eql("${JAVA_HOME:-#{f.opt_libexec}}")
+      expect(overridable_java_home_env[:JAVA_HOME]).to include(f.opt_libexec.to_s)
     end
   end
 end
