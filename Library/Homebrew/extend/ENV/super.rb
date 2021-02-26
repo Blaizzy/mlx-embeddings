65c03573c65f2655d7f93853fa632600bf58b58d
Bo Anderson <mail@boanderson.me>
2021-02-26 05:10:32 +0000

    ENV: fix missing arg for generic OS and correct type sigs

--- Library/Homebrew/extend/ENV/super.rb
+++ Library/Homebrew/extend/ENV/super.rb
@@ -44,13 +44,14 @@ module Superenv
   # @private
   sig {
     params(
-      formula:      T.nilable(Formula),
-      cc:           T.nilable(String),
-      build_bottle: T.nilable(T::Boolean),
-      bottle_arch:  T.nilable(T::Boolean),
+      formula:         T.nilable(Formula),
+      cc:              T.nilable(String),
+      build_bottle:    T.nilable(T::Boolean),
+      bottle_arch:     T.nilable(String),
+      testing_formula: T::Boolean,
     ).void
   }
-  def setup_build_environment(formula: nil, cc: nil, build_bottle: false, bottle_arch: nil)
+  def setup_build_environment(formula: nil, cc: nil, build_bottle: false, bottle_arch: nil, testing_formula: false)
     super
     send(compiler)
 
