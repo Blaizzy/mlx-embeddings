169d8aca4cd81f6ac07cd748525b892398126c42
Markus Reiter <me@reitermark.us>
2021-03-22 04:15:06 +0100

    Only raise `Timeout::Error` if `timeout` is specified.

--- Library/Homebrew/utils/curl.rb
+++ Library/Homebrew/utils/curl.rb
@@ -85,7 +85,7 @@ module Utils
 
       return result if result.success? || !args.exclude?("--http1.1")
 
-      raise Timeout::Error, result.stderr.chomp if result.status.exitstatus == 28
+      raise Timeout::Error, result.stderr.lines.last.chomp if timeout && result.status.exitstatus == 28
 
       # Error in the HTTP2 framing layer
       if result.status.exitstatus == 16
