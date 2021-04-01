7f5a97d1f3674c889d2d62c03e5b4e558d2ad771
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-04-01 23:28:33 +0900

    change directory for log

--- Library/Homebrew/dev-cmd/tests.rb
+++ Library/Homebrew/dev-cmd/tests.rb
@@ -99,7 +99,7 @@ module Homebrew
       end
 
       parallel_rspec_log_path =if ENV["CI"]
-        "/tests/parallel_runtime_rspec.log"
+        "/tmp/parallel_runtime_rspec.log"
       else
         "#{HOMEBREW_CACHE}/tests/parallel_runtime_rspec.log"
       end
