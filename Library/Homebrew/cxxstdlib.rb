04abc51d1f2c8b0ab35bfd340afe5414925b7ac8
Mike McQuaid <mike@mikemcquaid.com>
2021-04-02 12:38:39 +0100

    Cleanup use of CxxStdlib
    - remove usage on macOS as we don't care about it there
    - don't error out on incompatibility but still store stdlib on Linux
    - remove (now) unused methods

--- Library/Homebrew/cxxstdlib.rb
+++ Library/Homebrew/cxxstdlib.rb
@@ -22,52 +22,14 @@ class CxxStdlib
   def self.create(type, compiler)
     raise ArgumentError, "Invalid C++ stdlib type: #{type}" if type && [:libstdcxx, :libcxx].exclude?(type)
 
-    apple_compiler = !compiler.to_s.match?(GNU_GCC_REGEXP)
-    CxxStdlib.new(type, compiler, apple_compiler)
-  end
-
-  def self.check_compatibility(formula, deps, keg, compiler)
-    return if formula.skip_cxxstdlib_check?
-
-    stdlib = create(keg.detect_cxx_stdlibs.first, compiler)
-
-    begin
-      stdlib.check_dependencies(formula, deps)
-    rescue CompatibilityError => e
-      opoo e.message
-    end
+    CxxStdlib.new(type, compiler)
   end
 
   attr_reader :type, :compiler
 
-  def initialize(type, compiler, apple_compiler)
+  def initialize(type, compiler)
     @type = type
     @compiler = compiler.to_sym
-    @apple_compiler = apple_compiler
-  end
-
-  # If either package doesn't use C++, all is well.
-  # libstdc++ and libc++ aren't ever intercompatible.
-  # libstdc++ is compatible across Apple compilers, but
-  # not between Apple and GNU compilers, nor between GNU compiler versions.
-  def compatible_with?(other)
-    return true if type.nil? || other.type.nil?
-
-    return false unless type == other.type
-
-    apple_compiler? && other.apple_compiler? ||
-      !other.apple_compiler? && compiler.to_s[4..6] == other.compiler.to_s[4..6]
-  end
-
-  def check_dependencies(formula, deps)
-    deps.each do |dep|
-      # Software is unlikely to link against libraries from build-time deps, so
-      # it doesn't matter if they link against different C++ stdlibs.
-      next if dep.build?
-
-      dep_stdlib = Tab.for_formula(dep.to_formula).cxxstdlib
-      raise CompatibilityError.new(formula, dep, dep_stdlib) unless compatible_with? dep_stdlib
-    end
   end
 
   def type_string
@@ -77,9 +39,5 @@ class CxxStdlib
   sig { returns(String) }
   def inspect
     "#<#{self.class.name}: #{compiler} #{type}>"
-  end
-
-  def apple_compiler?
-    @apple_compiler
   end
 end
