ab04cfed831cf82232f4c1d815ada5f112133a71
Markus Reiter <me@reitermark.us>
2021-04-03 20:10:25 +0200

    Improve `_fetch` compatibility layer.

--- Library/Homebrew/compat/early/download_strategy.rb
+++ Library/Homebrew/compat/early/download_strategy.rb
@@ -2,21 +2,41 @@
 # frozen_string_literal: true
 
 class AbstractDownloadStrategy
-  module Compat
+  module CompatFetch
     def fetch(timeout: nil)
       super()
+    end
+  end
+
+  module Compat_Fetch # rubocop:disable Naming/ClassAndModuleCamelCase
+    def _fetch(*args, **options)
+      options[:timeout] = nil unless options.key?(:timeout)
+
+      begin
+        super
+      rescue ArgumentError => e
+        raise unless e.message.include?("timeout")
+
+        odeprecated "`def _fetch` in a subclass of `CurlDownloadStrategy`"
+        options.delete(:timeout)
+        super(*args, **options)
+      end
     end
   end
 
   class << self
     def method_added(method)
       if method == :fetch && instance_method(method).arity.zero?
-        odeprecated "`def fetch` in a subclass of #{self}",
+        odeprecated "`def fetch` in a subclass of `#{self}`",
                     "`def fetch(timeout: nil, **options)` and output a warning " \
                     "when `options` contains new unhandled options"
 
         class_eval do
-          prepend Compat
+          prepend CompatFetch
+        end
+      elsif method == :_fetch
+        class_eval do
+          prepend Compat_Fetch
         end
       end
 
--- Library/Homebrew/compat/late.rb
+++ Library/Homebrew/compat/late.rb
@@ -1,4 +1,2 @@
 # typed: strict
 # frozen_string_literal: true
-
-require_relative "late/download_strategy"
