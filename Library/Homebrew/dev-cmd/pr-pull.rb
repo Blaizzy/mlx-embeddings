ffbabfd2380713f15579e8830249ec4aea12e180
Chris Wegrzyn <chris.wegrzyn@bluelabs.com>
2021-04-28 14:57:41 -0400

    Rename download-strategy to root-url-using

--- Library/Homebrew/dev-cmd/pr-pull.rb
+++ Library/Homebrew/dev-cmd/pr-pull.rb
@@ -61,8 +61,8 @@ module Homebrew
              description: "Target tap repository (default: `homebrew/core`)."
       flag   "--root-url=",
              description: "Use the specified <URL> as the root of the bottle's URL instead of Homebrew's default."
-      flag   "--download-strategy=",
-             description: "Use the specified download strategy for the root_url in the generated DSL"
+      flag   "--root-url-using=",
+             description: "Use the specified download strategy for the root_url in the generated DSL via `using:' arg"
       flag   "--bintray-mirror=",
              description: "Use the specified Bintray repository to automatically mirror stable URLs "\
                           "defined in the formulae (default: `mirror`)."
@@ -445,7 +445,7 @@ module Homebrew
           upload_args << "--warn-on-upload-failure" if args.warn_on_upload_failure?
           upload_args << "--committer=#{args.committer}" if args.committer
           upload_args << "--root-url=#{args.root_url}" if args.root_url
-          upload_args << "--download-strategy=#{args.download_strategy}" if args.download_strategy
+          upload_args << "--root-url-using=#{args.root_url_using}" if args.root_url_using
           upload_args << if archive_item.present?
             "--archive-item=#{archive_item}"
           else
--- Library/Homebrew/dev-cmd/pr-upload.rb
+++ Library/Homebrew/dev-cmd/pr-upload.rb
@@ -40,8 +40,8 @@ module Homebrew
              description: "Upload to the specified GitHub organisation's GitHub Packages (default: `homebrew`)."
       flag   "--root-url=",
              description: "Use the specified <URL> as the root of the bottle's URL instead of Homebrew's default."
-      flag   "--download-strategy=",
-             description: "Use the specified download strategy for the root_url in the generated DSL"
+      flag   "--root-url-using=",
+             description: "Use the specified download strategy for the root_url in the generated DSL via `using:' arg"
 
       named_args :none
     end
@@ -116,7 +116,7 @@ module Homebrew
     bottle_args << "--root-url=#{args.root_url}" if args.root_url
     bottle_args << "--committer=#{args.committer}" if args.committer
     bottle_args << "--no-commit" if args.no_commit?
-    bottle_args << "--download-strategy=#{args.download_strategy}" if args.download_strategy
+    bottle_args << "--root-url-using=#{args.root_url_using}" if args.root_url_using
     bottle_args += json_files
 
     if args.dry_run?
