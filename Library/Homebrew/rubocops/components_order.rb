5af7521ab82a149e045e77ef6992b341865fc30f
danielnachun <daniel.nachun@gmail.com>
2021-05-30 19:03:04 -0700

    components_order: allow keg_only in on_macos/on_linux blocks

--- Library/Homebrew/rubocops/components_order.rb
+++ Library/Homebrew/rubocops/components_order.rb
@@ -110,7 +110,7 @@ module RuboCop
         end
 
         def check_on_os_block_content(component_precedence_list, on_os_block)
-          on_os_allowed_methods = %w[depends_on patch resource deprecate! disable! conflicts_with]
+          on_os_allowed_methods = %w[depends_on patch resource deprecate! disable! conflicts_with keg_only]
           _, offensive_node = check_order(component_precedence_list, on_os_block.body)
           component_problem(*offensive_node) if offensive_node
           child_nodes = on_os_block.body.begin_type? ? on_os_block.body.child_nodes : [on_os_block.body]
