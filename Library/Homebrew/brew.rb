5649d339d7861f197b06f7f35c8bfdf46fdd217e
Rylan Polster <rslpolster@gmail.com>
2021-07-14 11:01:46 -0400

    Show message when developer mode is turned on

--- Library/Homebrew/brew.rb
+++ Library/Homebrew/brew.rb
@@ -93,6 +93,13 @@ begin
     internal_cmd ||= begin
       internal_dev_cmd = Commands.valid_internal_dev_cmd?(cmd)
       if internal_dev_cmd && !Homebrew::EnvConfig.developer?
+        opoo <<~MESSAGE if ENV["HOMEBREW_DEV_CMD_RUN"].blank?
+          #{Tty.bold}#{cmd}#{Tty.reset} is a developer command, so
+          Homebrew's developer mode has been automatically turned on.
+          To turn developer mode off, run #{Tty.bold}brew developer off#{Tty.reset}
+
+        MESSAGE
+
         Homebrew::Settings.write "devcmdrun", true
         ENV["HOMEBREW_DEV_CMD_RUN"] = "1"
       end
