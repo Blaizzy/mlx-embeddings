0d7cc258f98222e40dd75428b83c9d9422788a20
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-09-29 14:49:22 -0700

    cxxstdlib: delete unused CompatibilityError class

--- Library/Homebrew/cxxstdlib.rb
+++ Library/Homebrew/cxxstdlib.rb
@@ -7,18 +7,6 @@ require "compilers"
 class CxxStdlib
   extend T::Sig
 
-  include CompilerConstants
-
-  # Error for when a formula's dependency was built with a different C++ standard library.
-  class CompatibilityError < StandardError
-    def initialize(formula, dep, stdlib)
-      super <<~EOS
-        #{formula.full_name} dependency #{dep.name} was built with a different C++ standard
-        library (#{stdlib.type_string} from #{stdlib.compiler}). This may cause problems at runtime.
-      EOS
-    end
-  end
-
   def self.create(type, compiler)
     raise ArgumentError, "Invalid C++ stdlib type: #{type}" if type && [:libstdcxx, :libcxx].exclude?(type)
 
