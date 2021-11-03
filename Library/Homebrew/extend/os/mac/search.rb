4c484ec6cb7bd19f3d86e442b0d8aedec32a34e2
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-03 16:47:22 -0700

    search: optimize cask search

--- Library/Homebrew/extend/os/mac/search.rb
+++ Library/Homebrew/extend/os/mac/search.rb
@@ -31,9 +31,16 @@ module Homebrew
           end
         end
 
-        results = Cask::Cask.search(string_or_regex, &:token).sort_by(&:token)
+        cask_tokens = Tap.flat_map(&:cask_tokens)
 
-        results.map do |cask|
+        results = cask_tokens.extend(Searchable)
+                             .search(string_or_regex)
+
+        results |= DidYouMean::SpellChecker.new(dictionary: cask_tokens)
+                                           .correct(string_or_regex)
+
+        results.sort.map do |name|
+          cask = Cask::CaskLoader.load(name)
           if cask.installed?
             pretty_installed(cask.token)
           else
