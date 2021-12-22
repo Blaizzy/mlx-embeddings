69feb81a5043cec274967785939be3419abcd284
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-12-22 18:07:18 +0000

    brew vendor-gems: commit updates.

--- Library/Homebrew/vendor/bundle/ruby/2.6.0/gems/did_you_mean-1.6.1/lib/did_you_mean/formatter.rb
+++ Library/Homebrew/vendor/bundle/ruby/2.6.0/gems/did_you_mean-1.6.1/lib/did_you_mean/formatter.rb
@@ -1,10 +1,10 @@
 # frozen-string-literal: true
 
 module DidYouMean
-  # The +DidYouMean::PlainFormatter+ is the basic, default formatter for the
+  # The +DidYouMean::Formatter+ is the basic, default formatter for the
   # gem. The formatter responds to the +message_for+ method and it returns a
   # human readable string.
-  class PlainFormatter
+  class Formatter
 
     # Returns a human readable string that contains +corrections+. This
     # formatter is designed to be less verbose to not take too much screen
@@ -12,7 +12,7 @@ module DidYouMean
     #
     # @example
     #
-    #   formatter = DidYouMean::PlainFormatter.new
+    #   formatter = DidYouMean::Formatter.new
     #
     #   # displays suggestions in two lines with the leading empty line
     #   puts formatter.message_for(["methods", "method"])
@@ -26,8 +26,19 @@ module DidYouMean
     #
     #   # => nil
     #
-    def message_for(corrections)
+    def self.message_for(corrections)
       corrections.empty? ? "" : "\nDid you mean?  #{corrections.join("\n               ")}"
     end
+
+    def message_for(corrections)
+      warn "The instance method #message_for has been deprecated. Please use the class method " \
+           "DidYouMean::Formatter.message_for(...) instead."
+
+      self.class.message_for(corrections)
+    end
   end
+
+  PlainFormatter = Formatter
+
+  deprecate_constant :PlainFormatter
 end
