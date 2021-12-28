65ddf196091f6cb6889e371c3aed6a0af1b7e168
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-12-28 11:48:58 +0900

    cmd/autoremove recognize cask dependencies

--- Library/Homebrew/cmd/autoremove.rb
+++ Library/Homebrew/cmd/autoremove.rb
@@ -36,6 +36,10 @@ module Homebrew
     removable_formulae = get_removable_formulae(Formula.installed)
     return if removable_formulae.blank?
 
+    if Cask::Caskroom.casks.present?
+      removable_formulae -= Cask::Caskroom.casks.flat_map { |cask| cask.depends_on[:formula] }
+                                          .compact.map { |formula| Formula[formula] }
+    end
     formulae_names = removable_formulae.map(&:full_name).sort
 
     verb = args.dry_run? ? "Would uninstall" : "Uninstalling"
