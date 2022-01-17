974f356901628b114cb2ea70347fde73054f3193
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-01-17 19:34:29 +0000

    brew vendor-gems: commit updates.

--- Library/Homebrew/vendor/bundle/ruby/2.6.0/gems/rubocop-rails-2.13.2/lib/rubocop/cop/rails/compact_blank.rb
+++ Library/Homebrew/vendor/bundle/ruby/2.6.0/gems/rubocop-rails-2.13.2/lib/rubocop/cop/rails/compact_blank.rb
@@ -10,25 +10,21 @@ module RuboCop
       #   blank check of block arguments to the receiver object.
       #
       #   For example, `[[1, 2], [3, nil]].reject { |first, second| second.blank? }` and
-      #   `[[1, 2], [3, nil]].compact_blank` are not compatible. The same is true for `empty?`.
+      #   `[[1, 2], [3, nil]].compact_blank` are not compatible. The same is true for `blank?`.
       #   This will work fine when the receiver is a hash object.
       #
       # @example
       #
       #   # bad
       #   collection.reject(&:blank?)
-      #   collection.reject(&:empty?)
       #   collection.reject { |_k, v| v.blank? }
-      #   collection.reject { |_k, v| v.empty? }
       #
       #   # good
       #   collection.compact_blank
       #
       #   # bad
       #   collection.reject!(&:blank?)
-      #   collection.reject!(&:empty?)
       #   collection.reject! { |_k, v| v.blank? }
-      #   collection.reject! { |_k, v| v.empty? }
       #
       #   # good
       #   collection.compact_blank!
@@ -48,13 +44,13 @@ module RuboCop
             (send _ {:reject :reject!})
             $(args ...)
             (send
-              $(lvar _) {:blank? :empty?}))
+              $(lvar _) :blank?))
         PATTERN
 
         def_node_matcher :reject_with_block_pass?, <<~PATTERN
           (send _ {:reject :reject!}
             (block_pass
-              (sym {:blank? :empty?})))
+              (sym :blank?)))
         PATTERN
 
         def on_send(node)
@@ -73,10 +69,15 @@ module RuboCop
           return true if reject_with_block_pass?(node)
 
           if (arguments, receiver_in_block = reject_with_block?(node.parent))
-            return arguments.length == 1 || use_hash_value_block_argument?(arguments, receiver_in_block)
+            return use_single_value_block_argument?(arguments, receiver_in_block) ||
+                   use_hash_value_block_argument?(arguments, receiver_in_block)
           end
 
           false
+        end
+
+        def use_single_value_block_argument?(arguments, receiver_in_block)
+          arguments.length == 1 && arguments[0].source == receiver_in_block.source
         end
 
         def use_hash_value_block_argument?(arguments, receiver_in_block)
