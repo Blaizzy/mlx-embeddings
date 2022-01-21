f3fe865a94db314cd37543ec979c177e502b236f
Nanda H Krishna <me@nandahkrishna.com>
2022-01-21 06:18:38 -0500

    dev-cmd/bump-formula-pr: improve version error message

--- Library/Homebrew/dev-cmd/bump-formula-pr.rb
+++ Library/Homebrew/dev-cmd/bump-formula-pr.rb
@@ -408,7 +408,7 @@ module Homebrew
     resource.owner = Resource.new(formula.name)
     forced_version = new_version && new_version != resource.version
     resource.version = new_version if forced_version
-    odie "No `--version=` argument specified!" if resource.version.blank?
+    odie "Couldn't identify version, specify it using `--version=`." if resource.version.blank?
     [resource.fetch, forced_version]
   end
 
