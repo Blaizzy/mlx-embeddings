aa4ad964eb11fd0ae4a523bd5d05a1d7c716f37c
Bo Anderson <mail@boanderson.me>
2022-03-02 13:12:47 +0000

    unpack_strategy/zip: allow unzip formula to be used

--- Library/Homebrew/unpack_strategy/zip.rb
+++ Library/Homebrew/unpack_strategy/zip.rb
@@ -27,10 +27,17 @@ module UnpackStrategy
               .returns(SystemCommand::Result)
     }
     def extract_to_dir(unpack_dir, basename:, verbose:)
+      unzip = begin
+        Formula["unzip"]
+      rescue FormulaUnavailableError
+        nil
+      end
+
       with_env(TZ: "UTC") do
         quiet_flags = verbose ? [] : ["-qq"]
         result = system_command! "unzip",
                                  args:         [*quiet_flags, "-o", path, "-d", unpack_dir],
+                                 env:          { "PATH" => PATH.new(unzip&.opt_bin, ENV["PATH"]) },
                                  verbose:      verbose,
                                  print_stderr: false
 
