6daa22bc8ee1d6353bb80a54042dcb5fb4b3a1b2
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-08-01 23:34:53 +0200

    Fixed issue with `print_latest_version`

--- Library/Homebrew/livecheck/livecheck.rb
+++ Library/Homebrew/livecheck/livecheck.rb
@@ -504,7 +504,7 @@ module Homebrew
     # Formats and prints the livecheck result for a formula/cask/resource.
     sig { params(info: Hash, verbose: T::Boolean, ambiguous_cask: T::Boolean, check_resource: T::Boolean).void }
     def print_latest_version(info, verbose:, ambiguous_cask: false, check_resource: false)
-      package_or_resource_s = "  " if check_resource
+      package_or_resource_s = check_resource ? "  " : ""
       package_or_resource_s += "#{Tty.blue}#{info[:formula] || info[:cask] || info[:resource]}#{Tty.reset}"
       package_or_resource_s += " (cask)" if ambiguous_cask && !check_resource
       package_or_resource_s += " (guessed)" if !info[:meta][:livecheckable] && !check_resource && verbose
