88a69b3de20e0faa12f8dee2c2298617d76b55ec
Lukas Oberhuber <lukaso@gmail.com>
2022-08-02 08:47:19 -0700

    Restore previous style

--- Library/Homebrew/build.rb
+++ Library/Homebrew/build.rb
@@ -78,8 +78,13 @@ class Build
       ENV.keg_only_deps = keg_only_deps
       ENV.deps = formula_deps
       ENV.run_time_deps = run_time_deps
-      ENV.setup_build_environment(formula: formula, cc: args.cc, build_bottle:  args.build_bottle?,
-                                  bottle_arch: args.bottle_arch, debug_symbols: args.debug_symbols?)
+      ENV.setup_build_environment(
+        formula:       formula,
+        cc:            args.cc,
+        build_bottle:  args.build_bottle?,
+        bottle_arch:   args.bottle_arch,
+        debug_symbols: args.debug_symbols?,
+      )
       reqs.each do |req|
         req.modify_build_environment(
           env: args.env, cc: args.cc, build_bottle: args.build_bottle?, bottle_arch: args.bottle_arch,
@@ -87,8 +92,13 @@ class Build
       end
       deps.each(&:modify_build_environment)
     else
-      ENV.setup_build_environment(formula: formula, cc: args.cc, build_bottle: args.build_bottle?,
-                                  bottle_arch: args.bottle_arch, debug_symbols: args.debug_symbols?)
+      ENV.setup_build_environment(
+        formula:       formula,
+        cc:            args.cc,
+        build_bottle:  args.build_bottle?,
+        bottle_arch:   args.bottle_arch,
+        debug_symbols: args.debug_symbols?,
+      )
       reqs.each do |req|
         req.modify_build_environment(
           env: args.env, cc: args.cc, build_bottle: args.build_bottle?, bottle_arch: args.bottle_arch,
