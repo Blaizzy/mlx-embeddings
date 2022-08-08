95080ebbb7f8293741172dd655bac3d37d6a389b
Sean Molenaar <SMillerDev@users.noreply.github.com>
2022-08-08 16:18:06 +0200

    inreplace: style fixes
    Co-authored-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Homebrew/utils/inreplace.rb
+++ Library/Homebrew/utils/inreplace.rb
@@ -70,7 +70,7 @@ module Utils
         Pathname(path).atomic_write(s.inreplace_string)
       end
 
-      raise Utils::Inreplace::Error, errors unless errors.empty?
+      raise Utils::Inreplace::Error, errors if errors.present?
     end
 
     # @api private
@@ -86,7 +86,7 @@ module Utils
 
         contents.gsub!(old, new)
       end
-      raise Utils::Inreplace::Error, path => contents.errors unless contents.errors.empty?
+      raise Utils::Inreplace::Error, path => contents.errors if contents.errors.present?
 
       Pathname(path).atomic_write(contents.inreplace_string) unless read_only_run
       contents.inreplace_string
