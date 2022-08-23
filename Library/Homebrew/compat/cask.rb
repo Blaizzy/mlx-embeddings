e7aa71de26884832e4ea32aad2a8619916380eb7
Mike McQuaid <mike@mikemcquaid.com>
2022-08-23 11:25:02 +0100

    Remove odisabled for 3.6.0
    The next release will be 3.6.0.

--- Library/Homebrew/compat/cask.rb
+++ Library/Homebrew/compat/cask.rb
@@ -1,21 +1,2 @@
 # typed: true
 # frozen_string_literal: true
-
-module Cask
-  class Cask
-    extend Enumerable
-
-    def self.each(&block)
-      odisabled "`Enumerable` methods on `Cask::Cask`",
-                "`Cask::Cask.all` (but avoid looping over all casks, it's slow and insecure)"
-
-      return to_enum unless block
-
-      Tap.flat_map(&:cask_files).each do |f|
-        yield CaskLoader::FromTapPathLoader.new(f).load(config: nil)
-      rescue CaskUnreadableError => e
-        opoo e.message
-      end
-    end
-  end
-end
