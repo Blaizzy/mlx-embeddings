1fae6062e61746df459ab7fb8f85e8f982c38d60
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-08-25 23:57:04 +0800

    Fix check for tap git directory.
    Co-authored-by: Rylan Polster <rslpolster@gmail.com>

--- Library/Homebrew/formula_auditor.rb
+++ Library/Homebrew/formula_auditor.rb
@@ -407,7 +407,7 @@ module Homebrew
 
     def audit_gcc_dependency
       return unless @core_tap
-      return if !@strict && !@git && !formula.tap.git? # git log is required for non-strict audit
+      return if !@strict && !(@git && formula.tap.git?) # git log is required for non-strict audit
       return unless Homebrew::SimulateSystem.simulating_or_running_on_linux?
       return unless linux_only_gcc_dep?(formula)
 
