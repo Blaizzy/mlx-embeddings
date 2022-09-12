dbb9af49e7ed20ca23e14ab90710a6c334804bd7
Mike McQuaid <mike@mikemcquaid.com>
2022-09-12 14:25:34 +0100

    Update Library/Homebrew/cmd/docs.rb
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Library/Homebrew/cmd/docs.rb
+++ Library/Homebrew/cmd/docs.rb
@@ -12,7 +12,7 @@ module Homebrew
   def docs_args
     Homebrew::CLI::Parser.new do
       description <<~EOS
-        Open Homebrew's online documentation (https://docs.brew.sh) in a browser.
+        Open Homebrew's online documentation (#{HOMEBREW_DOCS_WWW}) in a browser.
       EOS
     end
   end
