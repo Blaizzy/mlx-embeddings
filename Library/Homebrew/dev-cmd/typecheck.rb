250f1882c87268a5af94e98d11a0e3cc6b1cc99a
Issy Long <me@issyl0.co.uk>
2022-10-11 09:58:44 +0100

    dev-cmd/typecheck: Reinstate `srb rbi hidden-definitions`
    - Turns out we do need this. I lulled us into a false sense of security
      by not deleting `sorbet/rbi/hidden-defintions/hidden.rbi` previously.
    - If we do delete that file then Sorbet reveals >200 typechecking
      issues.
    - The docs on moving from `srb rbi` to `tapioca` say that we don't need
      `hidden-definitions` anymore, but clearly we do. The blog post goes
      into a lot of detail on why `rbi hidden-defintions` exists and the
      fact that there's not an analogous command in Tapioca:
      https://sorbet.org/blog/2022/07/27/srb-tapioca#whats-happening-with-srb-rbi-hidden-definitions
    - Eventually we should try out `tapioca dsl`, since that's said to be a
      "partial replacement" for `hidden-definitions`, and removing our
      reliance on deprecated commands can only be a good thing?

--- Library/Homebrew/dev-cmd/typecheck.rb
+++ Library/Homebrew/dev-cmd/typecheck.rb
@@ -67,6 +67,7 @@ module Homebrew
         ohai "Updating Tapioca RBI files..."
         safe_system "bundle", "exec", "tapioca", "gem", *tapioca_args
         safe_system "bundle", "exec", "parlour"
+        safe_system "bundle", "exec", "srb", "rbi", "hidden-definitions"
         safe_system "bundle", "exec", "tapioca", "todo"
 
         if args.suggest_typed?
