d1be288418342ab0b7825abe933c40c7001dee6e
Mike McQuaid <mike@mikemcquaid.com>
2023-01-20 17:41:07 +0000

    some needs_network specs: temporarily disable.
    These are absurdly flaky right now so let's disable them.

--- Library/Homebrew/test/cmd/search_spec.rb
+++ Library/Homebrew/test/cmd/search_spec.rb
@@ -7,14 +7,17 @@ require "cmd/shared_examples/args_parse"
 describe "brew search" do
   it_behaves_like "parseable arguments"
 
-  it "falls back to a GitHub tap search when no formula is found", :integration_test, :needs_macos, :needs_network do
-    setup_test_formula "testball"
-    setup_remote_tap "homebrew/cask"
+  # TODO: consider re-enabling when/if we can make these not flaky.
+  #
+  # it "falls back to a GitHub tap search when no formula is found",
+  #    :integration_test, :needs_macos, :needs_network do
+  #   setup_test_formula "testball"
+  #   setup_remote_tap "homebrew/cask"
 
-    expect { brew "search", "homebrew/cask/firefox" }
-      .to output(/firefox/).to_stdout
-      .and be_a_success
-  end
+  #   expect { brew "search", "homebrew/cask/firefox" }
+  #     .to output(/firefox/).to_stdout
+  #     .and be_a_success
+  # end
 
   # doesn't actually need Linux but only want one integration test per-OS.
   it "finds formula in search", :integration_test, :need_linux do
--- Library/Homebrew/test/utils/repology_spec.rb
+++ Library/Homebrew/test/utils/repology_spec.rb
@@ -2,31 +2,3 @@
 # frozen_string_literal: true
 
 require "utils/repology"
-
-describe Repology do
-  describe "single_package_query", :needs_homebrew_curl, :needs_network do
-    it "returns nil for non-existent package" do
-      response = described_class.single_package_query("invalidName", repository: "homebrew")
-
-      expect(response).to be_nil
-    end
-
-    it "returns a hash for existing package" do
-      response = described_class.single_package_query("openclonk", repository: "homebrew")
-
-      expect(response).not_to be_nil
-      expect(response).to be_a(Hash)
-    end
-  end
-
-  describe "parse_api_response", :needs_homebrew_curl, :needs_network do
-    it "returns a hash of data" do
-      limit = 1
-      start_with = "x"
-      response = described_class.parse_api_response(limit, start_with, repository: "homebrew")
-
-      expect(response).not_to be_nil
-      expect(response).to be_a(Hash)
-    end
-  end
-end
