82890894828db0b379e740b16d10512f53721584
hyuraku <32809703+hyuraku@users.noreply.github.com>
2023-01-24 21:57:39 +0900

    add remove_const to cancel warning

--- Library/Homebrew/extend/os/mac/default_prefix.rb
+++ Library/Homebrew/extend/os/mac/default_prefix.rb
@@ -2,6 +2,9 @@
 # frozen_string_literal: true
 
 module Homebrew
+  remove_const(:DEFAULT_PREFIX)
   DEFAULT_PREFIX = HOMEBREW_MACOS_ARM_DEFAULT_PREFIX
+
+  remove_const(:DEFAULT_REPOSITORY)
   DEFAULT_REPOSITORY = HOMEBREW_MACOS_ARM_DEFAULT_REPOSITORY
 end
