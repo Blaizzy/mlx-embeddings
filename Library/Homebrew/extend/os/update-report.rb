653930bed82858f14d49182aa7feedfccfbc4f09
hyuraku <32809703+hyuraku@users.noreply.github.com>
2023-01-28 21:38:40 +0900

    mv files to os/*/cmd/

--- Library/Homebrew/extend/os/update-report.rb
+++ Library/Homebrew/extend/os/update-report.rb
@@ -1,4 +1,8 @@
 # typed: strict
 # frozen_string_literal: true
 
-require "extend/os/mac/update-report" if OS.mac?
+if OS.mac?
+  require "extend/os/mac/cmd/update-report"
+elsif OS.linux?
+  require "extend/os/linux/cmd/update-report"
+end
