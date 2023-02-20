57844530a94d5686029c6770d97583f2bc7b4e9c
Mike McQuaid <mike@mikemcquaid.com>
2023-02-20 09:05:15 +0000

    analytics: remove UUID.
    We don't use this at all with InfluxDB and don't need it any more for GA
    so let's just remove it.

--- Library/Homebrew/utils/analytics.sh
+++ Library/Homebrew/utils/analytics.sh
@@ -1,28 +1,23 @@
-# Migrate analytics UUID to its new home in Homebrew repo's git config and
-# remove the legacy UUID file if detected.
+# Setup analytics and delete old analytics UUIDs.
 # HOMEBREW_LINUX, HOMEBREW_REPOSITORY is set by bin/brew
 # HOMEBREW_NO_ANALYTICS is from the user environment.
 # shellcheck disable=SC2154
-migrate-legacy-uuid-file() {
-  local legacy_uuid_file analytics_uuid
-
-  legacy_uuid_file="${HOME}/.homebrew_analytics_user_uuid"
-
-  if [[ -f "${legacy_uuid_file}" ]]
-  then
-    analytics_uuid="$(cat "${legacy_uuid_file}")"
-    if [[ -n "${analytics_uuid}" ]]
-    then
-      git config --file="${HOMEBREW_REPOSITORY}/.git/config" --replace-all homebrew.analyticsuuid "${analytics_uuid}" 2>/dev/null
-    fi
-    rm -f "${legacy_uuid_file}"
-  fi
-}
 
 setup-analytics() {
   local git_config_file="${HOMEBREW_REPOSITORY}/.git/config"
 
-  migrate-legacy-uuid-file
+  local legacy_uuid_file="${HOME}/.homebrew_analytics_user_uuid"
+  if [[ -f "${legacy_uuid_file}" ]]
+  then
+    rm -f "${legacy_uuid_file}"
+  fi
+
+  local user_uuid
+  user_uuid="$(git config --file="${git_config_file}" --get homebrew.analyticsuuid 2>/dev/null)"
+  if [[ -n "${user_uuid}" ]]
+  then
+    git config --file="${git_config_file}" --unset-all homebrew.analyticsuuid 2>/dev/null
+  fi
 
   if [[ -n "${HOMEBREW_NO_ANALYTICS}" ]]
   then
@@ -37,28 +32,6 @@ setup-analytics() {
     # Internal variable for brew's use, to differentiate from user-supplied setting
     export HOMEBREW_NO_ANALYTICS_THIS_RUN="1"
     return
-  fi
-
-  HOMEBREW_ANALYTICS_USER_UUID="$(git config --file="${git_config_file}" --get homebrew.analyticsuuid 2>/dev/null)"
-  if [[ -z "${HOMEBREW_ANALYTICS_USER_UUID}" ]]
-  then
-    if [[ -x /usr/bin/uuidgen ]]
-    then
-      HOMEBREW_ANALYTICS_USER_UUID="$(/usr/bin/uuidgen)"
-    elif [[ -r /proc/sys/kernel/random/uuid ]]
-    then
-      HOMEBREW_ANALYTICS_USER_UUID="$(tr a-f A-F </proc/sys/kernel/random/uuid)"
-    else
-      HOMEBREW_ANALYTICS_USER_UUID="$(uuidgen)"
-    fi
-
-    if [[ -z "${HOMEBREW_ANALYTICS_USER_UUID}" ]]
-    then
-      # Avoid sending bogus analytics if no UUID could be generated.
-      export HOMEBREW_NO_ANALYTICS_THIS_RUN="1"
-      return
-    fi
-    git config --file="${git_config_file}" --replace-all homebrew.analyticsuuid "${HOMEBREW_ANALYTICS_USER_UUID}" 2>/dev/null
   fi
 
   if [[ -n "${HOMEBREW_LINUX}" ]]
