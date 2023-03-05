1295904986b13abc0e4cfc4a0d616d36b4783490
Issy Long <me@issyl0.co.uk>
2023-03-05 23:57:56 +0000

    sorbet: Autogenerate the RBI file for `utils/tty.rb`
    - Sort of. It's not via Parlour, because I read the docs and all it says
      about dynamically generating things is "of course it's better to do
      that", no examples. I tried my best, but Bo's work on `attr.rb` was
      mind-boggling.
    - Instead, let's add a simple but functional generator script that I
      actually understand, as an alternative to maintaining these RBI files
      for dynamic methods manually, so that we can get rid of some use of
      `srb rbi hidden-definitions` since that's deprecated.
    - TODO: Run this script as part of `brew typecheck --update` so it gets
      updated with all the other RBI files periodically.
    - Follow up to PR 14651.

---
+++ Library/Homebrew/sorbet/custom_generators/tty.rb
@@ -0,0 +1,30 @@
+# typed: true
+# frozen_string_literal: true
+
+require_relative "../../utils/tty"
+
+File.open("#{File.dirname(__FILE__)}/../../utils/tty.rbi", "w") do |file|
+  file.write(<<~RUBY)
+    # typed: strict
+    # frozen_string_literal: true
+
+    module Tty
+  RUBY
+
+  dynamic_methods = Tty::COLOR_CODES.keys + Tty::STYLE_CODES.keys + Tty::SPECIAL_CODES.keys
+  methods = Tty.methods(false).sort.select { |method| dynamic_methods.include?(method) }
+
+  methods.each do |method|
+    return_type = (method.to_s.end_with?("?") ? T::Boolean : String)
+    signature = "sig { returns(#{return_type}) }"
+
+    file.write(<<-RUBY)
+  #{signature}
+  def self.#{method}; end
+    RUBY
+
+    file.write("\n") unless methods.last == method
+  end
+
+  file.write("end\n")
+end
