1e95de4757f4cb6117160df73c94f64a780be8eb
Wes Higbee <wes.mcclure@gmail.com>
2023-03-06 07:51:36 -0600

    split up suggestions

--- Library/Homebrew/dev-cmd/cat.rb
+++ Library/Homebrew/dev-cmd/cat.rb
@@ -53,7 +53,8 @@ module Homebrew
     end
 
     if Homebrew.failed?
-      $stderr.puts "The name may be wrong, or the tap hasn't been tapped. Consider using `brew info --github formula|cask [...]` instead."
+      $stderr.puts "The name may be wrong, or the tap hasn't been tapped."
+      $stderr.puts "Consider using `brew info --github formula|cask [...]` instead."
       return
     end
 
