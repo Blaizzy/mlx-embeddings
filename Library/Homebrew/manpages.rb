13b6825c2d487f47b680e074fa2d64367afddb20
Douglas Eichelberger <dduugg@gmail.com>
2023-03-08 09:50:04 -0800

    Fix invalid locale error

--- Library/Homebrew/manpages.rb
+++ Library/Homebrew/manpages.rb
@@ -5,6 +5,9 @@ require "cli/parser"
 require "erb"
 require "i18n"
 
+I18n.backend.available_locales # Initialize locales so they can be overwritten.
+I18n.backend.store_translations :en, support: { array: { last_word_connector: " and " } }
+
 SOURCE_PATH = (HOMEBREW_LIBRARY_PATH/"manpages").freeze
 TARGET_MAN_PATH = (HOMEBREW_REPOSITORY/"manpages").freeze
 TARGET_DOC_PATH = (HOMEBREW_REPOSITORY/"docs").freeze
