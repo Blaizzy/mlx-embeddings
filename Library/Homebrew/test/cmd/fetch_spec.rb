4271d41e51a58fcf72244eb07d8d06cdd4072c1a
hyuraku <32809703+hyuraku@users.noreply.github.com>
2023-03-19 22:31:00 +0900

    remove new specs

--- Library/Homebrew/test/cmd/fetch_spec.rb
+++ Library/Homebrew/test/cmd/fetch_spec.rb
@@ -4,10 +4,6 @@
 require "cmd/shared_examples/args_parse"
 
 describe "brew fetch" do
-  let(:local_transmission) do
-    Cask::CaskLoader.load(cask_path("local-transmission"))
-  end
-
   it_behaves_like "parseable arguments"
 
   it "downloads the Formula's URL", :integration_test do
@@ -17,30 +13,5 @@ describe "brew fetch" do
 
     expect(HOMEBREW_CACHE/"testball--0.1.tbz").to be_a_symlink
     expect(HOMEBREW_CACHE/"testball--0.1.tbz").to exist
-  end
-
-  it "prevents double fetch (without nuking existing installation)", :integration_test do
-    setup_test_formula "testball"
-    brew "fetch", "testball"
-
-    old_ctime = File.stat(HOMEBREW_CACHE/"testball--0.1.tbz").ctime
-
-    expect { brew "fetch", "testball", "--no-quarantine" }.to be_a_success
-    new_ctime = File.stat(HOMEBREW_CACHE/"testball--0.1.tbz").ctime
-
-    expect(old_ctime.to_i).to eq(new_ctime.to_i)
-  end
-
-  it "allows double fetch with --force", :integration_test do
-    setup_test_formula "testball"
-    brew "fetch", "testball"
-
-    old_ctime = File.stat(HOMEBREW_CACHE/"testball--0.1.tbz").ctime
-    sleep(1)
-
-    expect { brew "fetch", "testball", "--force", "--no-quarantine" }.to be_a_success
-    new_ctime = File.stat(HOMEBREW_CACHE/"testball--0.1.tbz").ctime
-
-    expect(new_ctime.to_i).to be > old_ctime.to_i
   end
 end
