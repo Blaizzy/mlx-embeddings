1e18e88c689aca0cd520b213c6c28f8802b89671
Issy Long <me@issyl0.co.uk>
2023-03-20 00:38:56 +0000

    test/fixtures: Fix style for the new `Cask/NoOverrides` cop

--- Library/Homebrew/test/support/fixtures/cask/Casks/multiple-versions.rb
+++ Library/Homebrew/test/support/fixtures/cask/Casks/multiple-versions.rb
@@ -2,9 +2,10 @@ cask "multiple-versions" do
   arch arm: "arm", intel: "intel"
   platform = on_arch_conditional arm: "darwin-arm64", intel: "darwin"
 
-  version "1.2.3"
-  sha256 "67cdb8a02803ef37fdbf7e0be205863172e41a561ca446cd84f0d7ab35a99d94"
-
+  on_monterey :or_newer do
+    version "1.2.3"
+    sha256 "67cdb8a02803ef37fdbf7e0be205863172e41a561ca446cd84f0d7ab35a99d94"
+  end
   on_big_sur do
     version "1.2.0"
     sha256 "8c62a2b791cf5f0da6066a0a4b6e85f62949cd60975da062df44adf887f4370b"
