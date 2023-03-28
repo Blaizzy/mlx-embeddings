46ac052b3b60d342cd6b9790dd3efe51060810dd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-03-28 22:01:45 +0800

    dev-cmd/pr-automerge: skip PRs labelled `CI-published-bottle-commits`
    This is based on feedback from @plinnane on Slack.

--- Library/Homebrew/dev-cmd/pr-automerge.rb
+++ Library/Homebrew/dev-cmd/pr-automerge.rb
@@ -23,7 +23,7 @@ module Homebrew
              description: "Pull requests must have this label."
       comma_array "--without-labels",
                   description: "Pull requests must not have these labels (default: " \
-                               "`do not merge`, `new formula`, `automerge-skip`)."
+                               "`do not merge`, `new formula`, `automerge-skip`, `CI-published-bottle-commits`)."
       switch "--without-approval",
              description: "Pull requests do not require approval to be merged."
       switch "--publish",
@@ -51,6 +51,7 @@ module Homebrew
       "do not merge",
       "new formula",
       "automerge-skip",
+      "CI-published-bottle-commits",
     ]
     tap = Tap.fetch(args.tap || CoreTap.instance.name)
 
