7b336ed8cd3eb20461fdc45ce67e5318e53920c4
Mike McQuaid <mike@mikemcquaid.com>
2023-04-04 08:49:27 +0100

    Revert "Split prof gems into their own group"

--- Library/Homebrew/dev-cmd/prof.rb
+++ Library/Homebrew/dev-cmd/prof.rb
@@ -24,8 +24,6 @@ module Homebrew
   def prof
     args = prof_args.parse
 
-    Homebrew.install_bundler_gems!(groups: ["prof"])
-
     brew_rb = (HOMEBREW_LIBRARY_PATH/"brew.rb").resolved_path
     FileUtils.mkdir_p "prof"
     cmd = args.named.first
@@ -43,12 +41,16 @@ module Homebrew
     end
 
     if args.stackprof?
+      # Already installed from Gemfile but use this to setup PATH and LOADPATH
+      Homebrew.install_gem_setup_path! "stackprof"
       with_env HOMEBREW_STACKPROF: "1" do
         system(*HOMEBREW_RUBY_EXEC_ARGS, brew_rb, *args.named)
       end
       output_filename = "prof/d3-flamegraph.html"
       safe_system "stackprof --d3-flamegraph prof/stackprof.dump > #{output_filename}"
     else
+      # Already installed from Gemfile but use this to setup PATH and LOADPATH
+      Homebrew.install_gem_setup_path! "ruby-prof"
       output_filename = "prof/call_stack.html"
       safe_system "ruby-prof", "--printer=call_stack", "--file=#{output_filename}", brew_rb, "--", *args.named
     end
