5b8ead7bc80312c6759c03638c022c34365c9a47
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-04-07 22:21:46 +0800

    Add more tests
    Also, remove most integration tests, because they're slow.

--- Library/Homebrew/test/dev-cmd/determine-test-runners_spec.rb
+++ Library/Homebrew/test/dev-cmd/determine-test-runners_spec.rb
@@ -9,12 +9,7 @@ describe "brew determine-test-runners" do
     FileUtils.rm_f github_output
   end
 
-  # TODO: Generate this dynamically based on our supported macOS versions.
   let(:linux_runner) { "ubuntu-22.04" }
-  let(:all_runners) { ["11", "11-arm64", "12", "12-arm64", "13", "13-arm64", linux_runner] }
-  let(:intel_runners) { all_runners.reject { |r| r.end_with? "-arm64" } }
-  let(:arm64_runners) { all_runners - intel_runners }
-  let(:macos_runners) { all_runners - [linux_runner] }
   # We need to make sure we write to a different path for each example.
   let(:github_output) { "#{TEST_TMPDIR}/github_output#{DetermineRunnerTestHelper.new.number}" }
   let(:ephemeral_suffix) { "-12345-1" }
@@ -26,27 +21,22 @@ describe "brew determine-test-runners" do
       "GITHUB_RUN_ATTEMPT"     => ephemeral_suffix.split("-").third,
     }.freeze
   end
+  let(:all_runners) do
+    out = []
+    MacOSVersions::SYMBOLS.each_value do |v|
+      macos_version = OS::Mac::Version.new(v)
+      next if macos_version.unsupported_release?
+
+      out << v
+      out << "#{v}-arm64"
+    end
+
+    out << linux_runner
+
+    out
+  end
 
   it_behaves_like "parseable arguments"
-
-  it "fails without any arguments", :integration_test do
-    expect { brew "determine-test-runners" }
-      .to not_to_output.to_stdout
-      .and be_a_failure
-  end
-
-  it "fails when the necessary environment variables are missing", :integration_test do
-    setup_test_formula "testball"
-
-    runner_env.each_key do |k|
-      runner_env_dup = runner_env.dup
-      runner_env_dup[k] = nil
-
-      expect { brew "determine-test-runners", "testball", runner_env_dup }
-        .to not_to_output.to_stdout
-        .and be_a_failure
-    end
-  end
 
   it "assigns all runners for formulae without any requirements", :integration_test do
     setup_test_formula "testball"
@@ -57,125 +47,7 @@ describe "brew determine-test-runners" do
       .and be_a_success
 
     expect(File.read(github_output)).not_to be_empty
-    expect(get_runners(github_output)).to eq(all_runners)
-  end
-
-  it "assigns all runners when there are deleted formulae", :integration_test do
-    expect { brew "determine-test-runners", "", "testball", runner_env.merge({ "GITHUB_OUTPUT" => github_output }) }
-      .to not_to_output.to_stdout
-      .and not_to_output.to_stderr
-      .and be_a_success
-
-    expect(File.read(github_output)).not_to be_empty
-    expect(get_runners(github_output)).to eq(all_runners)
-  end
-
-  it "assigns `ubuntu-latest` when there are no testing formulae and no deleted formulae", :integration_test do
-    expect { brew "determine-test-runners", "", runner_env.merge({ "GITHUB_OUTPUT" => github_output }) }
-      .to not_to_output.to_stdout
-      .and not_to_output.to_stderr
-      .and be_a_success
-
-    expect(File.read(github_output)).not_to be_empty
-    expect(get_runners(github_output)).to eq(["ubuntu-latest"])
-  end
-
-  it "assigns only Intel runners when a formula `depends_on arch: :x86_64`", :integration_test do
-    setup_test_formula "intel_depender", <<~RUBY
-      url "https://brew.sh/intel_depender-1.0.tar.gz"
-      depends_on arch: :x86_64
-    RUBY
-
-    expect { brew "determine-test-runners", "intel_depender", runner_env.merge({ "GITHUB_OUTPUT" => github_output }) }
-      .to not_to_output.to_stdout
-      .and not_to_output.to_stderr
-      .and be_a_success
-
-    expect(File.read(github_output)).not_to be_empty
-    expect(get_runners(github_output)).to eq(intel_runners)
-  end
-
-  it "assigns only ARM64 runners when a formula `depends_on arch: :arm64`", :integration_test do
-    setup_test_formula "fancy-m1-ml-framework", <<~RUBY
-      url "https://brew.sh/fancy-m1-ml-framework-1.0.tar.gz"
-      depends_on arch: :arm64
-    RUBY
-
-    expect do
-      brew "determine-test-runners", "fancy-m1-ml-framework", runner_env.merge({ "GITHUB_OUTPUT" => github_output })
-    end
-      .to not_to_output.to_stdout
-      .and not_to_output.to_stderr
-      .and be_a_success
-
-    expect(File.read(github_output)).not_to be_empty
-    expect(get_runners(github_output)).to eq(arm64_runners)
-  end
-
-  it "assigns only macOS runners when a formula `depends_on :macos`", :integration_test do
-    setup_test_formula "xcode-helper", <<~RUBY
-      url "https://brew.sh/xcode-helper-1.0.tar.gz"
-      depends_on :macos
-    RUBY
-
-    expect { brew "determine-test-runners", "xcode-helper", runner_env.merge({ "GITHUB_OUTPUT" => github_output }) }
-      .to not_to_output.to_stdout
-      .and not_to_output.to_stderr
-      .and be_a_success
-
-    expect(File.read(github_output)).not_to be_empty
-    expect(get_runners(github_output)).to eq(macos_runners)
-  end
-
-  it "assigns only Linux runners when a formula `depends_on :linux`", :integration_test do
-    setup_test_formula "linux-kernel-requirer", <<~RUBY
-      url "https://brew.sh/linux-kernel-requirer-1.0.tar.gz"
-      depends_on :linux
-    RUBY
-
-    expect do
-      brew "determine-test-runners", "linux-kernel-requirer", runner_env.merge({ "GITHUB_OUTPUT" => github_output })
-    end
-      .to not_to_output.to_stdout
-      .and not_to_output.to_stderr
-      .and be_a_success
-
-    expect(File.read(github_output)).not_to be_empty
-    expect(get_runners(github_output)).to eq([linux_runner])
-  end
-
-  # TODO: Keep this updated to use the newest supported macOS version.
-  it "assigns only compatible runners when there is a versioned macOS requirement", :integration_test do
-    setup_test_formula "needs-macos-13", <<~RUBY
-      url "https://brew.sh/needs-macos-13-1.0.tar.gz"
-      depends_on macos: :ventura
-    RUBY
-
-    expect { brew "determine-test-runners", "needs-macos-13", runner_env.merge({ "GITHUB_OUTPUT" => github_output }) }
-      .to not_to_output.to_stdout
-      .and not_to_output.to_stderr
-      .and be_a_success
-
-    expect(File.read(github_output)).not_to be_empty
-    expect(get_runners(github_output)).to eq(["13", "13-arm64", linux_runner])
-    expect(get_runners(github_output)).not_to eq(all_runners)
-  end
-
-  describe "--dependents" do
-    it "assignes no runners for formulae with no dependents", :integration_test do
-      setup_test_formula "testball"
-
-      expect do
-        brew "determine-test-runners", "--eval-all", "--dependents", "testball",
-             runner_env.merge({ "GITHUB_OUTPUT" => github_output })
-      end
-        .to not_to_output.to_stdout
-        .and not_to_output.to_stderr
-        .and be_a_success
-
-      expect(File.read(github_output)).not_to be_empty
-      expect(get_runners(github_output)).to eq([])
-    end
+    expect(get_runners(github_output).sort).to eq(all_runners.sort)
   end
 end
 
