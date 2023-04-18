7058d2e1d675d63c67901eb25111726b09062b7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-04-18 18:13:00 +0000

    brew vendor-gems: commit updates.

--- Library/Homebrew/vendor/bundle/ruby/2.6.0/gems/rubocop-rspec-2.20.0/lib/rubocop/cop/rspec/match_array.rb
+++ Library/Homebrew/vendor/bundle/ruby/2.6.0/gems/rubocop-rspec-2.20.0/lib/rubocop/cop/rspec/match_array.rb
@@ -3,7 +3,11 @@
 module RuboCop
   module Cop
     module RSpec
-      # Prefer `contain_exactly` when matching an array literal.
+      # Checks where `match_array` is used.
+      #
+      # This cop checks for the following:
+      # - Prefer `contain_exactly` when matching an array with values.
+      # - Prefer `eq` when using `match_array` with an empty array literal.
       #
       # @example
       #   # bad
@@ -17,20 +21,34 @@ module RuboCop
       #
       #   # good
       #   it { is_expected.to match_array(%w(tremble in fear foolish mortals)) }
+      #
       class MatchArray < Base
         extend AutoCorrector
 
         MSG = 'Prefer `contain_exactly` when matching an array literal.'
         RESTRICT_ON_SEND = %i[match_array].freeze
 
+        # @!method match_array_with_empty_array?(node)
+        def_node_matcher :match_array_with_empty_array?, <<~PATTERN
+          (send nil? :match_array (array))
+        PATTERN
+
         def on_send(node)
           return unless node.first_argument.array_type?
+          return if match_array_with_empty_array?(node)
+
+          check_populated_array(node)
+        end
+
+        private
+
+        def check_populated_array(node)
           return if node.first_argument.percent_literal?
 
           add_offense(node) do |corrector|
             array_contents = node.arguments.flat_map(&:to_a)
             corrector.replace(
-              node.source_range,
+              node,
               "contain_exactly(#{array_contents.map(&:source).join(', ')})"
             )
           end
