8539607a89e08edd56ad498fe4bc46f0b64a4efe
Douglas Eichelberger <dduugg@gmail.com>
2023-04-25 11:02:31 -0700

    Organize activesupport requires

--- Library/Homebrew/global.rb
+++ Library/Homebrew/global.rb
@@ -12,17 +12,17 @@ require "set"
 
 # Only require "core_ext" here to ensure we're only requiring the minimum of
 # what we need.
-require "active_support/core_ext/object/blank"
-require "active_support/core_ext/string/filters"
-require "active_support/core_ext/object/try"
 require "active_support/core_ext/array/access"
-require "active_support/core_ext/hash/except"
-require "active_support/core_ext/kernel/reporting"
-require "active_support/core_ext/hash/keys"
-require "active_support/core_ext/hash/deep_merge"
-require "active_support/core_ext/file/atomic"
 require "active_support/core_ext/enumerable"
+require "active_support/core_ext/file/atomic"
+require "active_support/core_ext/hash/deep_merge"
+require "active_support/core_ext/hash/except"
+require "active_support/core_ext/hash/keys"
+require "active_support/core_ext/kernel/reporting"
+require "active_support/core_ext/object/blank"
+require "active_support/core_ext/object/try"
 require "active_support/core_ext/string/exclude"
+require "active_support/core_ext/string/filters"
 require "active_support/core_ext/string/indent"
 
 HOMEBREW_API_DEFAULT_DOMAIN = ENV.fetch("HOMEBREW_API_DEFAULT_DOMAIN").freeze
