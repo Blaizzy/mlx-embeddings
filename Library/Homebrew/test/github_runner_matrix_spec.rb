3bfe3e30371280c6a00b504f354d935f95a1b42b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-04-27 07:20:58 +0800

    github_runner_matrix: fix tests

--- Library/Homebrew/test/github_runner_matrix_spec.rb
+++ Library/Homebrew/test/github_runner_matrix_spec.rb
@@ -8,6 +8,7 @@ describe GitHubRunnerMatrix do
     allow(ENV).to receive(:fetch).with("HOMEBREW_LINUX_RUNNER").and_return("ubuntu-latest")
     allow(ENV).to receive(:fetch).with("HOMEBREW_LINUX_CLEANUP").and_return("false")
     allow(ENV).to receive(:fetch).with("HOMEBREW_MACOS_TIMEOUT").and_return("90")
+    allow(ENV).to receive(:fetch).with("HOMEBREW_MACOS_BUILD_ON_GITHUB_RUNNER", "false").and_return("false")
     allow(ENV).to receive(:fetch).with("GITHUB_RUN_ID").and_return("12345")
     allow(ENV).to receive(:fetch).with("GITHUB_RUN_ATTEMPT").and_return("1")
   end
