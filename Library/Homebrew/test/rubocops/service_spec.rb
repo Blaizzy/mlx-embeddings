254941c6f7cd875d3a7b9bacdcedad825203aa00
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-04-30 07:39:25 +0800

    rubocops/service: check for more cellar paths
    This is a follow-up to #15154. In addition to `bin`, the `service` block
    can also reference other cellar paths like `libexec`. They don't work
    with the API either, as reported in
    https://github.com/orgs/Homebrew/discussions/4459.
    
    Violations are already corrected in Homebrew/homebrew-core#129736
    and Homebrew/homebrew-core#129737.
    
    Signed-off-by: Ruoyu Zhong <zhongruoyu@outlook.com>

--- Library/Homebrew/test/rubocops/service_spec.rb
+++ Library/Homebrew/test/rubocops/service_spec.rb
@@ -5,7 +5,7 @@ require "rubocops/service"
 describe RuboCop::Cop::FormulaAudit::Service do
   subject(:cop) { described_class.new }
 
-  it "reports an offense when a formula's service block uses `bin`" do
+  it "reports offenses when a formula's service block uses cellar paths" do
     expect_offense(<<~RUBY)
       class Foo < Formula
         url "https://brew.sh/foo-1.0.tgz"
@@ -13,6 +13,8 @@ describe RuboCop::Cop::FormulaAudit::Service do
         service do
           run [bin/"foo", "run", "-config", etc/"foo/config.json"]
                ^^^ FormulaAudit/Service: Use `opt_bin` instead of `bin` in service blocks.
+          working_dir libexec
+                      ^^^^^^^ FormulaAudit/Service: Use `opt_libexec` instead of `libexec` in service blocks.
         end
       end
     RUBY
@@ -23,18 +25,20 @@ describe RuboCop::Cop::FormulaAudit::Service do
 
         service do
           run [opt_bin/"foo", "run", "-config", etc/"foo/config.json"]
+          working_dir opt_libexec
         end
       end
     RUBY
   end
 
-  it "reports no offenses when a formula's service block uses `opt_bin`" do
+  it "reports no offenses when a formula's service block only uses opt paths" do
     expect_no_offenses(<<~RUBY)
       class Bin < Formula
         url "https://brew.sh/foo-1.0.tgz"
 
         service do
           run [opt_bin/"bin", "run", "-config", etc/"bin/config.json"]
+          working_dir opt_libexec
         end
       end
     RUBY
