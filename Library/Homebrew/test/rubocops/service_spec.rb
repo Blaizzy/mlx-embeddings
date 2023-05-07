1bae81cba2da7b148811acec80358f29bbb9e346
apainintheneck <apainintheneck@gmail.com>
2023-05-07 12:51:46 -0700

    rubocops/service: add checks for custom service names
    It now checks for two new edge cases.
    1. Are the required methods defined in the block?
    2. Is the `run` command defined if we're using methods other
       than `service_name` or `plist_name`?
    
    It has also been updated to only get the list of method call nodes
    once instead of doing it repeatedly for each cellar path check.

--- Library/Homebrew/test/rubocops/service_spec.rb
+++ Library/Homebrew/test/rubocops/service_spec.rb
@@ -5,6 +5,48 @@ require "rubocops/service"
 describe RuboCop::Cop::FormulaAudit::Service do
   subject(:cop) { described_class.new }
 
+  it "reports offenses when a service block is missing a required command" do
+    expect_offense(<<~RUBY)
+      class Foo < Formula
+        url "https://brew.sh/foo-1.0.tgz"
+
+        service do
+        ^^^^^^^^^^ FormulaAudit/Service: Service blocks require `run`, `plist_name` or `service_name` to be defined.
+          run_type :cron
+          working_dir "/tmp/example"
+        end
+      end
+    RUBY
+  end
+
+  it "reports no offenses when a service block only includes custom names" do
+    expect_no_offenses(<<~RUBY)
+      class Foo < Formula
+        url "https://brew.sh/foo-1.0.tgz"
+
+        service do
+          plist_name "custom.mcxl.foo"
+          service_name "custom.foo"
+        end
+      end
+    RUBY
+  end
+
+  it "reports offenses when a service block includes more than custom names and no run command" do
+    expect_offense(<<~RUBY)
+      class Foo < Formula
+        url "https://brew.sh/foo-1.0.tgz"
+
+        service do
+        ^^^^^^^^^^ FormulaAudit/Service: `run` must be defined to use methods other than `service_name` and `plist_name` like [:working_dir].
+          plist_name "custom.mcxl.foo"
+          service_name "custom.foo"
+          working_dir "/tmp/example"
+        end
+      end
+    RUBY
+  end
+
   it "reports offenses when a formula's service block uses cellar paths" do
     expect_offense(<<~RUBY)
       class Foo < Formula
@@ -31,7 +73,7 @@ describe RuboCop::Cop::FormulaAudit::Service do
     RUBY
   end
 
-  it "reports no offenses when a formula's service block only uses opt paths" do
+  it "reports no offenses when a service block only uses opt paths" do
     expect_no_offenses(<<~RUBY)
       class Bin < Formula
         url "https://brew.sh/foo-1.0.tgz"
