5c41e85ae9335610cbbf47f1c23b3a20f00f06c8
apainintheneck <apainintheneck@gmail.com>
2023-08-27 17:03:02 -0700

    update cmd/uses and cmd/deps tests
    These tests were very simple before and now this should result
    in more code coverage without affecting test performance.
    
    The only tricky thing was testing the `--missing` option without
    actually installing a package using `install_test_formula` because
    that is very slow (around 10 seconds on my machine). I ended
    up just writing the tab to a plausible keg directory for each
    package I wanted to "install". This allows us to test the behavior
    while also not increasing CI time by ~20 seconds (though it'd
    probably be faster on CI than my local machine).

--- Library/Homebrew/test/support/helper/spec/shared_context/integration_test.rb
+++ Library/Homebrew/test/support/helper/spec/shared_context/integration_test.rb
@@ -166,10 +166,6 @@ RSpec.shared_context "integration test" do # rubocop:disable RSpec/ContextWordin
 
         # something here
       RUBY
-    when "foo", "gnupg"
-      content = <<~RUBY
-        url "https://brew.sh/#{name}-1.0"
-      RUBY
     when "bar"
       content = <<~RUBY
         url "https://brew.sh/#{name}-1.0"
@@ -179,6 +175,10 @@ RSpec.shared_context "integration test" do # rubocop:disable RSpec/ContextWordin
       content = <<~RUBY
         url "https://brew.sh/#patchelf-1.0"
         license "0BSD"
+      RUBY
+    else
+      content ||= <<~RUBY
+        url "https://brew.sh/#{name}-1.0"
       RUBY
     end
 
