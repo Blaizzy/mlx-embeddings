94ef9a4b7aeb343dcfb5ac2c13e34eac3e7805f5
Steve Peters <scpeters@openrobotics.org>
2023-09-08 00:52:35 -0700

    codesign in keg_relocate instead of keg methods
    Currently the codesign_patched_binary method may be called many
    times for the same file when installing a keg.
    
    This removes the calls to codesign_patched_binary from os/mac/keg
    and adds a single call to the relocate_dynamic_linkage and
    fix_dynamic_linkage methods in extend/os/mac/keg_relocate
    to speed up keg installation.

--- Library/Homebrew/os/mac/keg.rb
+++ Library/Homebrew/os/mac/keg.rb
@@ -2,13 +2,14 @@
 # frozen_string_literal: true
 
 class Keg
+  sig { params(id: String, file: Pathname).returns(T::Boolean) }
   def change_dylib_id(id, file)
-    return if file.dylib_id == id
+    return false if file.dylib_id == id
 
     @require_relocation = true
     odebug "Changing dylib ID of #{file}\n  from #{file.dylib_id}\n    to #{id}"
     file.change_dylib_id(id, strict: false)
-    codesign_patched_binary(file)
+    true
   rescue MachO::MachOError
     onoe <<~EOS
       Failed changing dylib ID of #{file}
@@ -18,13 +19,14 @@ class Keg
     raise
   end
 
+  sig { params(old: String, new: String, file: Pathname).returns(T::Boolean) }
   def change_install_name(old, new, file)
-    return if old == new
+    return false if old == new
 
     @require_relocation = true
     odebug "Changing install name in #{file}\n  from #{old}\n    to #{new}"
     file.change_install_name(old, new, strict: false)
-    codesign_patched_binary(file)
+    true
   rescue MachO::MachOError
     onoe <<~EOS
       Failed changing install name in #{file}
@@ -34,13 +36,14 @@ class Keg
     raise
   end
 
+  sig { params(old: Pathname, new: Pathname, file: MachOShim).returns(T::Boolean) }
   def change_rpath(old, new, file)
-    return if old == new
+    return false if old == new
 
     @require_relocation = true
     odebug "Changing rpath in #{file}\n  from #{old}\n    to #{new}"
     file.change_rpath(old, new, strict: false)
-    codesign_patched_binary(file)
+    true
   rescue MachO::MachOError
     onoe <<~EOS
       Failed changing rpath in #{file}
@@ -50,10 +53,11 @@ class Keg
     raise
   end
 
+  sig { params(rpath: String, file: MachOShim).returns(T::Boolean) }
   def delete_rpath(rpath, file)
     odebug "Deleting rpath #{rpath} in #{file}"
     file.delete_rpath(rpath, strict: false)
-    codesign_patched_binary(file)
+    true
   rescue MachO::MachOError
     onoe <<~EOS
       Failed deleting rpath #{rpath} in #{file}
