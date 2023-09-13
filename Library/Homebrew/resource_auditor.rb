e7c4d7ebeb9a4a160dda790ca6bc555be29918bb
Mike McQuaid <mike@mikemcquaid.com>
2023-09-13 08:51:54 +0100

    resource_auditor: handle potential nil case.

--- Library/Homebrew/resource_auditor.rb
+++ Library/Homebrew/resource_auditor.rb
@@ -105,7 +105,7 @@ module Homebrew
       return if name == owner.name # Skip the top-level package name as we only care about `resource "foo"` blocks.
 
       url =~ %r{/(?<package_name>[^/]+)-}
-      pypi_package_name = Regexp.last_match(:package_name).gsub(/[_.]/, "-")
+      pypi_package_name = Regexp.last_match(:package_name).to_s.gsub(/[_.]/, "-")
       return if name.casecmp(pypi_package_name).zero?
 
       problem "resource name should be `#{pypi_package_name}` to match the PyPI package name"
