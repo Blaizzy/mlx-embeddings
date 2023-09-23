a0a4fa4ed9e5a5ef79ed8ecaebd4236f1a6dbdc8
Anatoli Babenia <anatoli@rainforce.org>
2023-09-23 13:01:43 +0300

    dev-cmd/edit: ignore linefeed in the message

--- Library/Homebrew/dev-cmd/edit.rb
+++ Library/Homebrew/dev-cmd/edit.rb
@@ -63,7 +63,7 @@ module Homebrew
              !Homebrew::EnvConfig.no_install_from_api? &&
              !Homebrew::EnvConfig.no_env_hints?
             opoo <<~EOS
-              `brew install` ignores locally edited #{(core_cask_path || core_cask_tap) ? "casks" : "formulae"},
+              `brew install` ignores locally edited #{(core_cask_path || core_cask_tap) ? "casks" : "formulae"} \
               if `HOMEBREW_NO_INSTALL_FROM_API` is not set.
             EOS
             edit_api_message_displayed = true
