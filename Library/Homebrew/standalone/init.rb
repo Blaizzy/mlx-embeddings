daa49a1323ccbd832e76acac9f22619fe209e52b
Bo Anderson <mail@boanderson.me>
2023-09-29 01:34:39 +0100

    Install gems automatically on newer Ruby

--- Library/Homebrew/standalone/init.rb
+++ Library/Homebrew/standalone/init.rb
@@ -1,5 +1,21 @@
-# typed: true
+# typed: false
 # frozen_string_literal: true
+
+# This file is included before any other files. It intentionally has typing disabled and has minimal use of `require`.
+
+required_ruby_major, required_ruby_minor, = ENV.fetch("HOMEBREW_REQUIRED_RUBY_VERSION", "").split(".").map(&:to_i)
+unsupported_ruby = if required_ruby_minor.nil?
+  # We're probably only running rubocop etc so just assume supported
+  false
+else
+  ruby_major, ruby_minor, = RUBY_VERSION.split(".").map(&:to_i)
+  if ruby_major < required_ruby_major || (ruby_major == required_ruby_major && ruby_minor < required_ruby_minor)
+    raise "Homebrew must be run under Ruby #{required_ruby_major}.#{required_ruby_minor}! " \
+          "You're running #{RUBY_VERSION}."
+  end
+
+  ruby_major != required_ruby_major || ruby_minor != required_ruby_minor
+end.freeze
 
 # We trust base Ruby to provide what we need.
 # Don't look into the user-installed sitedir, which may contain older versions of RubyGems.
@@ -11,6 +27,9 @@ HOMEBREW_LIBRARY_PATH = Pathname(__dir__).parent.realpath.freeze
 
 require_relative "../utils/gems"
 Homebrew.setup_gem_environment!(setup_path: false)
+
+# Install gems for Rubies we don't vendor for.
+Homebrew.install_bundler_gems! if unsupported_ruby
 
 $LOAD_PATH.push HOMEBREW_LIBRARY_PATH.to_s unless $LOAD_PATH.include?(HOMEBREW_LIBRARY_PATH.to_s)
 require_relative "../vendor/bundle/bundler/setup"
