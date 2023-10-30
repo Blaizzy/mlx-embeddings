329f8c7ca32cb05e4bf95ff622c1df26d0bc37ba
Bo Anderson <mail@boanderson.me>
2023-10-30 18:57:01 +0000

    startup/bootsnap: slim load path cache

--- Library/Homebrew/startup/bootsnap.rb
+++ Library/Homebrew/startup/bootsnap.rb
@@ -32,8 +32,17 @@ if homebrew_bootsnap_enabled
     cache = ENV.fetch("HOMEBREW_CACHE", nil) || ENV.fetch("HOMEBREW_DEFAULT_CACHE", nil)
     raise "Needs HOMEBREW_CACHE or HOMEBREW_DEFAULT_CACHE!" if cache.nil? || cache.empty?
 
+    # We never do `require "vendor/bundle/ruby/..."` or `require "vendor/portable-ruby/..."`,
+    # so let's slim the cache a bit by excluding them.
+    # Note that gems within `bundle/ruby` will still be cached - these are when directory walking down from above.
+    ignore_directories = [
+      (HOMEBREW_LIBRARY_PATH/"vendor/bundle/ruby").to_s,
+      (HOMEBREW_LIBRARY_PATH/"vendor/portable-ruby").to_s,
+    ]
+
     Bootsnap.setup(
       cache_dir:          cache,
+      ignore_directories: ignore_directories,
       load_path_cache:    true,
       compile_cache_iseq: true,
       compile_cache_yaml: true,
