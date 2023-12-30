b4657e1eef0099c9921b1fea510e50cc81686282
Michael Cho <michael@michaelcho.dev>
2023-12-30 12:30:40 -0500

    rubocops/components_redundancy: stable/head block removal
    When a `stable do` or `head do` block only contains `url`, `sha256`,
    `mirror`, and/or `version`, then the block should be removed.

--- Library/Homebrew/test/rubocops/components_redundancy_spec.rb
+++ Library/Homebrew/test/rubocops/components_redundancy_spec.rb
@@ -72,5 +72,47 @@ describe RuboCop::Cop::FormulaAudit::ComponentsRedundancy do
         end
       RUBY
     end
+
+    it "reports an offense if `stable do` or `head do` is present with only `url`" do
+      expect_offense(<<~RUBY)
+        class Foo < Formula
+          stable do
+          ^^^^^^^^^ FormulaAudit/ComponentsRedundancy: `stable do` should not be present with only url/sha256/mirror/version
+            url "https://brew.sh/foo-1.0.tgz"
+          end
+
+          head do
+          ^^^^^^^ FormulaAudit/ComponentsRedundancy: `head do` should not be present with only `url`
+            url "https://brew.sh/foo.git"
+          end
+        end
+      RUBY
+    end
+
+    it "reports no offenses if `stable do` is present with `url` and `depends_on`" do
+      expect_no_offenses(<<~RUBY)
+        class Foo < Formula
+          head "https://brew.sh/foo.git"
+
+          stable do
+            url "https://brew.sh/foo-1.0.tgz"
+            depends_on "bar"
+          end
+        end
+      RUBY
+    end
+
+    it "reports no offenses if `head do` is present with `url` and `depends_on`" do
+      expect_no_offenses(<<~RUBY)
+        class Foo < Formula
+          url "https://brew.sh/foo-1.0.tgz"
+
+          head do
+            url "https://brew.sh/foo.git"
+            depends_on "bar"
+          end
+        end
+      RUBY
+    end
   end
 end
