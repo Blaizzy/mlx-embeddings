db97ff2dce26be9ecd3d141d7529c028655ffee2
Douglas Eichelberger <dduugg@gmail.com>
2023-12-31 11:29:57 -0800

    Use define_method instead

--- Library/Homebrew/attrable.rb
+++ Library/Homebrew/attrable.rb
@@ -14,19 +14,9 @@ module Attrable
   sig { params(attrs: Symbol).void }
   def attr_rw(*attrs)
     attrs.each do |attr|
-      module_eval <<-EOS, __FILE__, __LINE__+1
-        def #{attr}(val=nil)                         # def prefix(val=nil)
-          if val.nil?                                #   if val.nil?
-            if instance_variable_defined?(:@#{attr}) #      if instance_variable_defined?(:@prefix)
-              return @#{attr}                        #        return @prefix
-            else                                     #      else
-              return nil                             #        return nil
-            end                                      #      end
-          end                                        #    end
-                                                     #
-          @#{attr} = val                             #   @prefix = val
-        end                                          # end
-      EOS
+      define_method attr do |val = nil|
+        val.nil? ? instance_variable_get(:"@#{attr}") : instance_variable_set(:"@#{attr}", val)
+      end
     end
   end
 end
