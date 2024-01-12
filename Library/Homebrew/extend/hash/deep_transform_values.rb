14f8a086d831ae710a16579abf73802782eeb1f5
Douglas Eichelberger <dduugg@gmail.com>
2024-01-11 20:04:14 -0800

    brew vendor-gems: commit updates.

--- Library/Homebrew/extend/hash/deep_transform_values.rb
+++ Library/Homebrew/extend/hash/deep_transform_values.rb
@@ -10,38 +10,35 @@ class Hash
   #
   #  hash.deep_transform_values{ |value| value.to_s.upcase }
   #  # => {person: {name: "ROB", age: "28"}}
-  def deep_transform_values(&block)
-    _deep_transform_values_in_object(self, &block)
-  end
+  def deep_transform_values(&block) = _deep_transform_values_in_object(self, &block)
 
   # Destructively converts all values by using the block operation.
   # This includes the values from the root hash and from all
   # nested hashes and arrays.
-  def deep_transform_values!(&block)
-    _deep_transform_values_in_object!(self, &block)
-  end
+  def deep_transform_values!(&block) = _deep_transform_values_in_object!(self, &block)
 
   private
-    # Support methods for deep transforming nested hashes and arrays.
-    def _deep_transform_values_in_object(object, &block)
-      case object
-      when Hash
-        object.transform_values { |value| _deep_transform_values_in_object(value, &block) }
-      when Array
-        object.map { |e| _deep_transform_values_in_object(e, &block) }
-      else
-        yield(object)
-      end
-    end
 
-    def _deep_transform_values_in_object!(object, &block)
-      case object
-      when Hash
-        object.transform_values! { |value| _deep_transform_values_in_object!(value, &block) }
-      when Array
-        object.map! { |e| _deep_transform_values_in_object!(e, &block) }
-      else
-        yield(object)
-      end
+  # Support methods for deep transforming nested hashes and arrays.
+  def _deep_transform_values_in_object(object, &block)
+    case object
+    when Hash
+      object.transform_values { |value| _deep_transform_values_in_object(value, &block) }
+    when Array
+      object.map { |e| _deep_transform_values_in_object(e, &block) }
+    else
+      yield(object)
     end
+  end
+
+  def _deep_transform_values_in_object!(object, &block)
+    case object
+    when Hash
+      object.transform_values! { |value| _deep_transform_values_in_object!(value, &block) }
+    when Array
+      object.map! { |e| _deep_transform_values_in_object!(e, &block) }
+    else
+      yield(object)
+    end
+  end
 end
