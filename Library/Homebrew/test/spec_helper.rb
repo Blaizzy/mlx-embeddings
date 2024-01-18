ec0434c54923400839f3d7712f9f87ddea7eb4e9
Bo Anderson <mail@boanderson.me>
2024-01-18 14:54:14 +0000

    Tweak library load path insertion to be as early as possible

--- Library/Homebrew/test/spec_helper.rb
+++ Library/Homebrew/test/spec_helper.rb
@@ -33,7 +33,7 @@ require "find"
 require "byebug"
 require "timeout"
 
-$LOAD_PATH.push(File.expand_path("#{ENV.fetch("HOMEBREW_LIBRARY")}/Homebrew/test/support/lib"))
+$LOAD_PATH.unshift(File.expand_path("#{ENV.fetch("HOMEBREW_LIBRARY")}/Homebrew/test/support/lib"))
 
 require_relative "../global"
 
