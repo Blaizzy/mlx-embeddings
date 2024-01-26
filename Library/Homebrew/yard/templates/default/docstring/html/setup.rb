36f64d6b3050fec2f972f1b6484607c8f8a76024
Douglas Eichelberger <dduugg@gmail.com>
2024-01-26 11:36:08 -0800

    Enable Style/TopLevelMethodDefinition

--- Library/Homebrew/yard/templates/default/docstring/html/setup.rb
+++ Library/Homebrew/yard/templates/default/docstring/html/setup.rb
@@ -1,6 +1,8 @@
 # typed: true
 # frozen_string_literal: true
 
+# This follows the docs at https://github.com/lsegal/yard/blob/main/docs/Templates.md#setuprb
+# rubocop:disable Style/TopLevelMethodDefinition
 def init
   # `sorbet` is available transitively through the `yard-sorbet` plugin, but we're
   # outside of the standalone sorbet config, so `checked` is enabled by default
@@ -16,3 +18,4 @@ def internal
   T.bind(self, YARD::Templates::Template, checked: false)
   erb(:internal) if object.has_tag?(:api) && object.tag(:api).text == "internal"
 end
+# rubocop:enable Style/TopLevelMethodDefinition
