ae249ec282c7552d5658c11a8310180c4ce43343
Douglas Eichelberger <dduugg@gmail.com>
2024-01-26 13:52:46 -0800

    Vendor InverseMethods cop

---
+++ Library/Homebrew/test/rubocops/negate_include_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "rubocops/negate_include"
+
+RSpec.describe RuboCop::Cop::Homebrew::NegateInclude, :config do
+  it "registers an offense and corrects when using `!include?`" do
+    expect_offense(<<~RUBY)
+      !array.include?(2)
+      ^^^^^^^^^^^^^^^^^^ Use `.exclude?` and remove the negation part.
+    RUBY
+
+    expect_correction(<<~RUBY)
+      array.exclude?(2)
+    RUBY
+  end
+
+  it "does not register an offense when using `!include?` without receiver" do
+    expect_no_offenses(<<~RUBY)
+      !include?(2)
+    RUBY
+  end
+
+  it "does not register an offense when using `include?` or `exclude?`" do
+    expect_no_offenses(<<~RUBY)
+      array.include?(2)
+      array.exclude?(2)
+    RUBY
+  end
+end
