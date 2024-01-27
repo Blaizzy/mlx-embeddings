9aabe1ba910127e582bcbcbef82a3b977c6cf1a1
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-01-27 16:19:16 -0500

    rubocop: preserve comments when ordering uninstall methods

--- Library/Homebrew/rubocops/cask/uninstall_methods_order.rb
+++ Library/Homebrew/rubocops/cask/uninstall_methods_order.rb
@@ -9,10 +9,9 @@ module RuboCop
       # This cop checks for the correct order of methods within the 'uninstall' and 'zap' stanzas.
       class UninstallMethodsOrder < Base
         extend AutoCorrector
-        include CaskHelp
         include HelperFunctions
 
-        MSG = T.let("`%<method>s` method out of order".freeze, String)
+        MSG = T.let("`%<method>s` method out of order", String)
 
         sig { params(node: AST::SendNode).void }
         def on_send(node)
@@ -23,18 +22,35 @@ module RuboCop
 
           method_nodes = hash_node.pairs.map(&:key)
           expected_order = method_nodes.sort_by { |method| method_order_index(method) }
+          comments = processed_source.comments
 
           method_nodes.each_with_index do |method, index|
             next if method == expected_order[index]
 
-            add_offense(method, message: format(MSG, method: method.children.first)) do |corrector|
-              indentation = " " * (start_column(method) - line_start_column(method))
-              ordered_sources = expected_order.map do |expected_method|
-                hash_node.pairs.find { |pair| pair.key == expected_method }.source
+            report_and_correct_offense(method, hash_node, expected_order, comments)
+          end
+        end
+
+        sig {
+          params(method:         AST::Node,
+                 hash_node:      AST::HashNode,
+                 expected_order: T::Array[AST::Node],
+                 comments:       T::Array[Parser::Source::Comment]).void
+        }
+        def report_and_correct_offense(method, hash_node, expected_order, comments)
+          add_offense(method, message: format(MSG, method: method.children.first)) do |corrector|
+            indentation = " " * (start_column(method) - line_start_column(method))
+            new_code = expected_order.map do |expected_method|
+              method_pair = hash_node.pairs.find { |pair| pair.key == expected_method }
+              source = method_pair.source
+
+              # Find and attach a comment on the same line as the method_pair, if any
+              inline_comment = comments.find do |comment|
+                comment.location.line == method_pair.loc.line && comment.location.column > method_pair.loc.column
               end
-              new_code = ordered_sources.join(",\n#{indentation}")
-              corrector.replace(hash_node.source_range, new_code)
-            end
+              inline_comment ? "#{source} #{inline_comment.text}" : source
+            end.join(",\n#{indentation}")
+            corrector.replace(hash_node.source_range, new_code)
           end
         end
 
