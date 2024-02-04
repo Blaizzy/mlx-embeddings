b010e397d68cd1c42df9a0c9d4c3ea929a1ca842
Douglas Eichelberger <dduugg@gmail.com>
2024-02-04 05:46:18 -0800

    Add extend/node.rbi, remove unused methods

--- Library/Homebrew/rubocops/cask/extend/node.rb
+++ Library/Homebrew/rubocops/cask/extend/node.rb
@@ -8,17 +8,11 @@ module RuboCop
       include RuboCop::Cask::Constants
 
       def_node_matcher :method_node, "{$(send ...) (block $(send ...) ...)}"
-      def_node_matcher :block_args,  "(block _ $_ _)"
       def_node_matcher :block_body,  "(block _ _ $_)"
-
-      def_node_matcher :key_node,    "{(pair $_ _) (hash (pair $_ _) ...)}"
-      def_node_matcher :val_node,    "{(pair _ $_) (hash (pair _ $_) ...)}"
-
       def_node_matcher :cask_block?, "(block (send nil? :cask ...) args ...)"
       def_node_matcher :on_system_block?,
                        "(block (send nil? {#{ON_SYSTEM_METHODS.map(&:inspect).join(" ")}} ...) args ...)"
       def_node_matcher :arch_variable?, "(lvasgn _ (send nil? :on_arch_conditional ...))"
-
       def_node_matcher :begin_block?, "(begin ...)"
 
       sig { returns(T::Boolean) }
