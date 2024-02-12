f2eb1564e4468e8242e951f241c8adc656f89183
Sean Molenaar <SMillerDev@users.noreply.github.com>
2024-02-12 16:12:52 +0100

    Update Library/Homebrew/utils/pypi.rb

--- Library/Homebrew/utils/pypi.rb
+++ Library/Homebrew/utils/pypi.rb
@@ -333,7 +333,7 @@ module PyPI
       EOS
     end
 
-    odie "Excluded superfluous packages: #{", ".join(exclude_packages)}" if exclude_packages.any?
+    odie "Excluded superfluous packages: #{exclude_packages.join(", ")}" if exclude_packages.any?
 
     if print_only
       puts new_resource_blocks.chomp
