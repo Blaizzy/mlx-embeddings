89b5914c865188b864d2034bf89fd2080c084613
Douglas Eichelberger <dduugg@gmail.com>
2024-02-17 10:04:39 -0800

    Add test that are exclusions are active dependencies

---
+++ Library/Homebrew/test/sorbet/tapioca/config_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "rubygems"
+require "yaml"
+
+describe "Tapioca Config" do
+  let(:config) { YAML.load_file(File.join(__dir__, "../../../sorbet/tapioca/config.yml")) }
+
+  it "only excludes dependencies" do
+    exclusions = config.dig("gem", "exclude")
+    dependencies = Gem::Specification.all.map(&:name)
+    expect(exclusions - dependencies).to be_empty
+  end
+end
