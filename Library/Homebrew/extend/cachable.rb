e9a05d896afddc610be860b531b332b0c7c79c9c
apainintheneck <apainintheneck@gmail.com>
2024-02-17 17:16:16 -0800

    cachable: make #overwrite_cache! private

--- Library/Homebrew/extend/cachable.rb
+++ Library/Homebrew/extend/cachable.rb
@@ -7,13 +7,15 @@ module Cachable
     @cache ||= T.let({}, T.nilable(T::Hash[T.untyped, T.untyped]))
   end
 
-  sig { params(hash: T.nilable(T::Hash[T.untyped, T.untyped])).void }
-  def overwrite_cache!(hash)
-    @cache = hash
-  end
-
   sig { void }
   def clear_cache
     cache.clear
+  end
+
+  private
+
+  sig { params(hash: T::Hash[T.untyped, T.untyped]).void }
+  def overwrite_cache!(hash)
+    @cache = hash
   end
 end
