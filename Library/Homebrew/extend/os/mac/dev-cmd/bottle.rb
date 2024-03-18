ee0c967ce0ec9fc44167636f4493a0683751439c
Douglas Eichelberger <dduugg@gmail.com>
2024-03-18 12:31:44 -0700

    Port Homebrew::DevCmd::Bottle

--- Library/Homebrew/extend/os/mac/dev-cmd/bottle.rb
+++ Library/Homebrew/extend/os/mac/dev-cmd/bottle.rb
@@ -2,23 +2,25 @@
 # frozen_string_literal: true
 
 module Homebrew
-  class << self
-    undef tar_args
+  module DevCmd
+    class Bottle < AbstractCommand
+      undef tar_args
 
-    sig { returns(T::Array[String]) }
-    def tar_args
-      if MacOS.version >= :catalina
-        ["--no-mac-metadata", "--no-acls", "--no-xattrs"].freeze
-      else
-        [].freeze
+      sig { returns(T::Array[String]) }
+      def tar_args
+        if MacOS.version >= :catalina
+          ["--no-mac-metadata", "--no-acls", "--no-xattrs"].freeze
+        else
+          [].freeze
+        end
       end
-    end
 
-    undef gnu_tar
+      undef gnu_tar
 
-    sig { params(gnu_tar_formula: Formula).returns(String) }
-    def gnu_tar(gnu_tar_formula)
-      "#{gnu_tar_formula.opt_bin}/gtar"
+      sig { params(gnu_tar_formula: Formula).returns(String) }
+      def gnu_tar(gnu_tar_formula)
+        "#{gnu_tar_formula.opt_bin}/gtar"
+      end
     end
   end
 end
