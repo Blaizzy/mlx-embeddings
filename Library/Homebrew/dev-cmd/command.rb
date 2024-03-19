777ee12258ec3a9d7ffa34dfe62454cd4da8bf05
Douglas Eichelberger <dduugg@gmail.com>
2024-03-19 11:50:53 -0700

    Port Homebrew::DevCmd::Command

--- Library/Homebrew/dev-cmd/command.rb
+++ Library/Homebrew/dev-cmd/command.rb
@@ -1,30 +1,29 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
+require "abstract_command"
 require "commands"
 require "cli/parser"
 
 module Homebrew
-  module_function
+  module DevCmd
+    class Command < AbstractCommand
+      cmd_args do
+        description <<~EOS
+          Display the path to the file being used when invoking `brew` <cmd>.
+        EOS
 
-  sig { returns(CLI::Parser) }
-  def command_args
-    Homebrew::CLI::Parser.new do
-      description <<~EOS
-        Display the path to the file being used when invoking `brew` <cmd>.
-      EOS
+        named_args :command, min: 1
+      end
 
-      named_args :command, min: 1
-    end
-  end
-
-  def command
-    args = command_args.parse
-
-    args.named.each do |cmd|
-      path = Commands.path(cmd)
-      odie "Unknown command: #{cmd}" unless path
-      puts path
+      sig { override.void }
+      def run
+        args.named.each do |cmd|
+          path = Commands.path(cmd)
+          odie "Unknown command: #{cmd}" unless path
+          puts path
+        end
+      end
     end
   end
 end
