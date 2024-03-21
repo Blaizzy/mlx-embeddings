afceaec076ece774672c83daf077e8d3f6f56954
Douglas Eichelberger <dduugg@gmail.com>
2024-03-21 10:18:05 -0700

    Port Homebrew::DevCmd::GenerateCaskApi

--- Library/Homebrew/test/dev-cmd/generate-cask-api_spec.rb
+++ Library/Homebrew/test/dev-cmd/generate-cask-api_spec.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require "cmd/shared_examples/args_parse"
+require "dev-cmd/generate-cask-api"
 
-RSpec.describe "brew generate-cask-api" do
+RSpec.describe Homebrew::DevCmd::GenerateCaskApi do
   it_behaves_like "parseable arguments"
 end
