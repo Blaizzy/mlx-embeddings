46d758d5cf792e7f591bec395faf4fe9aeb937d3
Douglas Eichelberger <dduugg@gmail.com>
2024-03-21 22:06:26 -0700

    Port Homebrew::DevCmd::UpdateLicenseData

--- Library/Homebrew/test/dev-cmd/update-license-data_spec.rb
+++ Library/Homebrew/test/dev-cmd/update-license-data_spec.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require "cmd/shared_examples/args_parse"
+require "dev-cmd/update-license-data"
 
-RSpec.describe "brew update-license-data" do
+RSpec.describe Homebrew::DevCmd::UpdateLicenseData do
   it_behaves_like "parseable arguments"
 end
