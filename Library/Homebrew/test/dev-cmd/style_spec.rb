0d04f198d2c8fd4c0782290c8c922c4803821bab
Douglas Eichelberger <dduugg@gmail.com>
2024-03-21 21:44:06 -0700

    Port Homebrew::DevCmd::StyleCmd

--- Library/Homebrew/test/dev-cmd/style_spec.rb
+++ Library/Homebrew/test/dev-cmd/style_spec.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require "cmd/shared_examples/args_parse"
+require "dev-cmd/style"
 
-RSpec.describe "brew style" do
+RSpec.describe Homebrew::DevCmd::StyleCmd do
   it_behaves_like "parseable arguments"
 end
