c789bf4475a419e9bdd5f2985c8b5ba4ad730233
Douglas Eichelberger <dduugg@gmail.com>
2024-03-21 22:12:37 -0700

    Port Homebrew::DevCmd::UpdateSponsors

--- Library/Homebrew/test/dev-cmd/update-sponsors_spec.rb
+++ Library/Homebrew/test/dev-cmd/update-sponsors_spec.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require "cmd/shared_examples/args_parse"
+require "dev-cmd/update-sponsors"
 
-RSpec.describe "brew update-sponsors" do
+RSpec.describe Homebrew::DevCmd::UpdateSponsors do
   it_behaves_like "parseable arguments"
 end
