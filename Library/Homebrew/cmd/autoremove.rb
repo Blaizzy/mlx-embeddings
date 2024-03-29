a8f8c65d93d0f4c0c4ba30ed059fcaebd4847984
Douglas Eichelberger <dduugg@gmail.com>
2024-03-29 16:20:40 -0700

    Port Homebrew::Cmd::Autoremove

--- Library/Homebrew/cmd/autoremove.rb
+++ Library/Homebrew/cmd/autoremove.rb
@@ -1,27 +1,26 @@
 # typed: strict
 # frozen_string_literal: true
 
+require "abstract_command"
 require "cleanup"
-require "cli/parser"
 
 module Homebrew
-  sig { returns(CLI::Parser) }
-  def self.autoremove_args
-    Homebrew::CLI::Parser.new do
-      description <<~EOS
-        Uninstall formulae that were only installed as a dependency of another formula and are now no longer needed.
-      EOS
-      switch "-n", "--dry-run",
-             description: "List what would be uninstalled, but do not actually uninstall anything."
+  module Cmd
+    class Autoremove < AbstractCommand
+      cmd_args do
+        description <<~EOS
+          Uninstall formulae that were only installed as a dependency of another formula and are now no longer needed.
+        EOS
+        switch "-n", "--dry-run",
+               description: "List what would be uninstalled, but do not actually uninstall anything."
 
-      named_args :none
+        named_args :none
+      end
+
+      sig { override.void }
+      def run
+        Cleanup.autoremove(dry_run: args.dry_run?)
+      end
     end
-  end
-
-  sig { void }
-  def self.autoremove
-    args = autoremove_args.parse
-
-    Cleanup.autoremove(dry_run: args.dry_run?)
   end
 end
