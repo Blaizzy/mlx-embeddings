db39e939b015c722a66e99a714b62a5f9fc2f864
Issy Long <me@issyl0.co.uk>
2024-03-30 13:07:16 +0000

    Apply suggestions from code review

--- Library/Homebrew/sorbet/tapioca/compilers/tty.rb
+++ Library/Homebrew/sorbet/tapioca/compilers/tty.rb
@@ -2,32 +2,26 @@
 # frozen_string_literal: true
 
 require_relative "../../../global"
-require_relative "../../../utils/tty"
+require "utils/tty"
 
 module Tapioca
   module Compilers
     class Tty < Tapioca::Dsl::Compiler
       # FIXME: Enable cop again when https://github.com/sorbet/sorbet/issues/3532 is fixed.
       # rubocop:disable Style/MutableConstant
-      # This should be a module whose singleton class contains RuboCop::AST::NodePattern::Macros,
-      #   but I don't know how to express that in Sorbet.
       ConstantType = type_member { { fixed: Module } }
       # rubocop:enable Style/MutableConstant
 
       sig { override.returns(T::Enumerable[Module]) }
-      def self.gather_constants
-        [::Tty]
-      end
+      def self.gather_constants = [::Tty]
 
       sig { override.void }
       def decorate
-        root.create_module(constant.name) do |mod|
+        root.create_module(T.must(constant.name)) do |mod|
           dynamic_methods = ::Tty::COLOR_CODES.keys + ::Tty::STYLE_CODES.keys + ::Tty::SPECIAL_CODES.keys
-          methods = ::Tty.methods(false).sort.select { |method| dynamic_methods.include?(method) }
 
-          methods.each do |method|
-            return_type = (method.to_s.end_with?("?") ? "T::Boolean" : "String")
-            mod.create_method(method.to_s, return_type:, class_method: true)
+          dynamic_methods.each do |method|
+            mod.create_method(method.to_s, return_type: "String", class_method: true)
           end
         end
       end
