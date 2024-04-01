c1b1c11a8c03fffe979856e9a6cd2d2dd8ae4cd9
Douglas Eichelberger <dduugg@gmail.com>
2024-04-01 09:44:14 -0700

    Port Homebrew::Cmd::Pin

--- Library/Homebrew/cmd/pin.rb
+++ Library/Homebrew/cmd/pin.rb
@@ -1,37 +1,35 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
+require "abstract_command"
 require "formula"
-require "cli/parser"
 
 module Homebrew
-  module_function
+  module Cmd
+    class Pin < AbstractCommand
+      cmd_args do
+        description <<~EOS
+          Pin the specified <formula>, preventing them from being upgraded when
+          issuing the `brew upgrade` <formula> command. See also `unpin`.
 
-  sig { returns(CLI::Parser) }
-  def pin_args
-    Homebrew::CLI::Parser.new do
-      description <<~EOS
-        Pin the specified <formula>, preventing them from being upgraded when
-        issuing the `brew upgrade` <formula> command. See also `unpin`.
+          *Note:* Other packages which depend on newer versions of a pinned formula
+          might not install or run correctly.
+        EOS
 
-        *Note:* Other packages which depend on newer versions of a pinned formula
-        might not install or run correctly.
-      EOS
+        named_args :installed_formula, min: 1
+      end
 
-      named_args :installed_formula, min: 1
-    end
-  end
-
-  def pin
-    args = pin_args.parse
-
-    args.named.to_resolved_formulae.each do |f|
-      if f.pinned?
-        opoo "#{f.name} already pinned"
-      elsif !f.pinnable?
-        onoe "#{f.name} not installed"
-      else
-        f.pin
+      sig { override.void }
+      def run
+        args.named.to_resolved_formulae.each do |f|
+          if f.pinned?
+            opoo "#{f.name} already pinned"
+          elsif !f.pinnable?
+            onoe "#{f.name} not installed"
+          else
+            f.pin
+          end
+        end
       end
     end
   end
