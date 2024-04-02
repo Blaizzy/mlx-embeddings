8ebcadd1c94d1f5c56c3e15aad71f66d35a55093
Douglas Eichelberger <dduugg@gmail.com>
2024-04-02 11:43:41 -0700

    Clean up args_parse tests

--- Library/Homebrew/test/cmd/services_spec.rb
+++ Library/Homebrew/test/cmd/services_spec.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-RSpec.describe "brew services", :integration_test, :needs_network do
-  it "allows controlling services" do
-    setup_remote_tap "homebrew/services"
+require "cmd/shared_examples/args_parse"
 
+RSpec.describe "Homebrew::Cmd::Services", :integration_test, :needs_network do
+  before { setup_remote_tap "homebrew/services" }
+
+  it_behaves_like "parseable arguments", command_name: "services"
+
+  it "allows controlling services" do
     expect { brew "services", "list" }
       .to not_to_output.to_stderr
       .and not_to_output.to_stdout
