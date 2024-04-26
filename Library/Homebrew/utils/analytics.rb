414e221990061893c95fb5d4798169733c30a84c
Mike McQuaid <mike@mikemcquaid.com>
2024-04-26 09:37:48 +0100

    utils/analytics: cleanup test-bot analytics.
    Sort options to ensure consistent ordering and improve readability.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Library/Homebrew/utils/analytics.rb
+++ Library/Homebrew/utils/analytics.rb
@@ -144,10 +144,23 @@ module Utils
           os:     HOMEBREW_SYSTEM,
         }
 
+        command_and_package, options =
+          step_command_short.split
+                            .partition { |arg| !arg.start_with?("-") }
+
         # Strip out any flag values to reduce cardinality and preserve privacy.
-        command = step_command_short.split
-                                    .map { |arg| arg.sub(/=.*/, "=") }
-                                    .join(" ")
+        # Sort options to ensure consistent ordering and improve readability.
+        options = options.join(" ")
+                         .split
+                         .join(" ")
+                         .split(/(-+)/)
+                         .map { |a| a.sub(/[ =].+/, " ") }
+                         .join(" ")
+                         .strip
+                         .split
+                         .sort
+
+        command = (command_and_package + options).join(" ")
 
         # Fields can have high cardinality.
         fields = { command: }
