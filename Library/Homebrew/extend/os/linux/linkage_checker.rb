cc65e5419da42afe94537c68ecb2d689c55e620f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-05-16 00:47:21 +0800

    linkage_checker: remove `gcc` from `@indirect_deps` on Linux
    Closes #17303.

--- Library/Homebrew/extend/os/linux/linkage_checker.rb
+++ Library/Homebrew/extend/os/linux/linkage_checker.rb
@@ -45,5 +45,6 @@ class LinkageChecker
     #   https://github.com/Homebrew/brew/pull/13796
     # TODO: Find a nicer way to handle this. (e.g. examining the ELF file to determine the required libstdc++.)
     @undeclared_deps.delete("gcc")
+    @indirect_deps.delete("gcc")
   end
 end
