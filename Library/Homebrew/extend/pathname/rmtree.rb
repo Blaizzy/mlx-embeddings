cc11da6bdd91c28bfff853926b28b17ab90950cb
Mike McQuaid <mike@mikemcquaid.com>
2024-05-17 13:14:30 +0900

    extend/pathname/rmtree: improve comment.
    Clarify that this can't easily be `odeprecated` but keep the wording
    around so that we remember to do this on the next minor release.
    
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Library/Homebrew/extend/pathname/rmtree.rb
+++ Library/Homebrew/extend/pathname/rmtree.rb
@@ -8,6 +8,10 @@ class Pathname
   #
   # @api public
   def rmtree(noop: nil, verbose: nil, secure: nil)
+    # Ideally we'd odeprecate this but probably can't given gems so let's
+    # create a RuboCop autocorrect instead soon.
+    # This is why monkeypatching is non-ideal (but right solution to get
+    # Ruby 3.3 over the line).
     # odeprecated "rmtree", "FileUtils#rm_r"
     FileUtils.rm_r(@path, noop:, verbose:, secure:)
     nil
