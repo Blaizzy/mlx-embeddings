a92e6fc1c8ed6fd7dabcd5aef18189cf9906ed16
Mike McQuaid <mike@mikemcquaid.com>
2024-05-18 08:16:34 +0100

    extend/pathname: import rmtree again.
    The separate file is no longer required now that
    https://github.com/sorbet/sorbet/pull/7895 was merged.

--- Library/Homebrew/extend/pathname.rb
+++ Library/Homebrew/extend/pathname.rb
@@ -498,6 +498,21 @@ class Pathname
                  .encode(Encoding::UTF_8, invalid: :replace)
                  .split("\n")
   end
+
+  # Like regular `rmtree`, except it never ignores errors.
+  #
+  # This was the default behaviour in Ruby 3.1 and earlier.
+  #
+  # @api public
+  def rmtree(noop: nil, verbose: nil, secure: nil)
+    # Ideally we'd odeprecate this but probably can't given gems so let's
+    # create a RuboCop autocorrect instead soon.
+    # This is why monkeypatching is non-ideal (but right solution to get
+    # Ruby 3.3 over the line).
+    # odeprecated "rmtree", "FileUtils#rm_r"
+    FileUtils.rm_r(@path, noop:, verbose:, secure:)
+    nil
+  end
 end
 
 require "extend/os/pathname"
@@ -584,5 +599,3 @@ module ObserverPathnameExtension
     puts "uninfo #{self}" if ObserverPathnameExtension.verbose?
   end
 end
-
-require "extend/pathname/rmtree"
