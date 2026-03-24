087a0551b22c616d1cc52d5bb91a101fa6e39cd4
Prince Canuma <prince.gdt@gmail.com>
2026-03-24 14:21:49 +0100

    format

--- mlx_embeddings/__init__.py
+++ mlx_embeddings/__init__.py
@@ -7,6 +7,6 @@ __email__ = "prince.gdt@gmail.com"
 
 os.environ.setdefault("TRANSFORMERS_NO_ADVISORY_WARNINGS", "1")
 
-from .version import __version__
 from .convert import convert
 from .utils import generate, load
+from .version import __version__
